fd186b33688706e316db6030f37d82cdc9ce30ad
Michael Pyle <mpyle101@gmail.com>
2022-12-07 17:39:24 -0700

    Don't need match/panic when unwrap accomplishes the same thing

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -18,13 +18,10 @@ macro_rules! priority {
     ($s1:expr, $($arr:expr),+) => {{
         let mut iter = $s1.chars();
         loop {
-            match iter.next() {
-                Some(b) => 
-                    if [$($arr),+].iter().all(|s| s.contains(b)) { 
-                        let c = b as u8;
-                        break (if c <= b'Z' { c - b'A' + 27 } else { c - b'a' + 1 }) as i32
-                    }, 
-                None => panic!("Common type not found!")
+            let b = iter.next().unwrap();
+            if [$($arr),+].iter().all(|s| s.contains(b)) { 
+                let c = b as u8;
+                break (if c <= b'Z' { c - b'A' + 27 } else { c - b'a' + 1 }) as i32
             }
         }
     }};
@@ -42,8 +39,7 @@ fn part_two(input: &str) -> i32 {
     let mut priorities = 0;
     let mut rucks = input.split('\n');
     while let Some(s1) = rucks.next() {
-        let s2 = rucks.next().unwrap();
-        let s3 = rucks.next().unwrap();
+        let (s2, s3) = (rucks.next().unwrap(), rucks.next().unwrap());
         priorities += priority!(s1, s2, s3);
         // OR: priorities += priority(s1, |c| s2.contains(c) && s3.contains(c))
     }

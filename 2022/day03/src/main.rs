5b8cf3816227b12b32797cdd8d80308f0839a509
Michael Pyle <mpyle101@gmail.com>
2022-12-07 17:39:24 -0700

    Variadic macro to compute priority

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -14,12 +14,30 @@ fn main() {
     println!("Part 2: {} ({:?})", calories, t.elapsed());
 }
 
+macro_rules! priority {
+    ($s1:expr, $($arr:expr),+) => {{
+        let mut iter = $s1.chars();
+        loop {
+            let c = match iter.next() {
+                Some(b) => 
+                    if [$($arr),+].iter().all(|s| s.contains(b)) { 
+                        b as u8 
+                    } else { 
+                        continue
+                    }, 
+                None => panic!("Common type not found!")
+            };
+
+            break (if c <= b'Z' { c - b'A' + 27 } else { c - b'a' + 1 }) as i32
+        }
+    }};
+}
+
 fn part_one(input: &str) -> i32 {
     input.split("\n")
         .fold(0, |acc, s| {
-            let (c1, c2) = s.split_at(s.len() / 2);
-            let c = find_common2(c1, c2);
-            acc + (if c <= b'Z' { c - b'A' + 27 } else { c - b'a' + 1 } as i32)
+            let (s1, s2) = s.split_at(s.len() / 2);
+            acc + priority!(s1, s2)
         })
 }
 
@@ -29,30 +47,10 @@ fn part_two(input: &str) -> i32 {
     while let Some(s1) = rucks.next() {
         let s2 = rucks.next().unwrap();
         let s3 = rucks.next().unwrap();
-        let c  = find_common3(s1, s2, s3);
-        priorities += (if c <= b'Z' { c - b'A' + 27 } else { c - b'a' + 1 } as i32);
+        priorities += priority!(s1, s2, s3);
     }
     
     priorities
-}
-
-fn find_common2(s1: &str, s2: &str) -> u8 {
-    let b2 = s2.as_bytes();
-    for b in s1.as_bytes() {
-        if b2.contains(b) { return *b }
-    }
-
-    panic!("Common type not found!")
-}
-
-fn find_common3(s1: &str, s2: &str, s3: &str) -> u8 {
-    let b2 = s2.as_bytes();
-    let b3 = s3.as_bytes();
-    for b in s1.as_bytes() {
-        if b2.contains(b) && b3.contains(b) { return *b }
-    }
-
-    panic!("Common type not found!")
 }
 
 

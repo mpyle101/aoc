6b22761fe0c4f41c316ca13c736e89ed1498e539
Michael Pyle <mpyle101@gmail.com>
2022-12-07 17:39:25 -0700

    Fill out

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -1,36 +1,58 @@
-use anyhow::Result;
 
 fn main() {
-  let total_fuel: i32 = include_str!("./modules.txt")
-    .lines()
-    .map(|l| l.parse::<i32>())
-    .map(Result::unwrap)
-    .map(fuel_needed)
-    .sum();
+    use std::time::Instant;
 
-  println!("Total fuel required: {}", total_fuel);
+    let input = include_str!("./modules.txt");
+
+    let t = Instant::now();
+    let fuel = part_one(input);
+    println!("Part 1: {} ({:?})", fuel, t.elapsed());
+
+    let t = Instant::now();
+    let fuel = part_two(input);
+    println!("Part 2: {} ({:?})", fuel, t.elapsed());
+}
+
+fn part_one(input: &str) -> i32 {
+    input.lines()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .map(|mass| mass / 3 - 2)
+        .sum()
+}
+
+fn part_two(input: &str) -> i32 {
+    input.lines()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .map(fuel_needed)
+        .sum()
 }
 
 fn fuel_needed(mass: i32) -> i32 {
-  let fuel = mass / 3 - 2;
-  if fuel <= 0 {
-    0
-  } else {
-    fuel + fuel_needed(fuel)
-  }
+    let fuel = mass / 3 - 2;
+    if fuel <= 0 { 0 } else { fuel + fuel_needed(fuel) }
 }
 
 
-/** Unit Tests */
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    assert_eq!(fuel_needed(12), 2);
-    assert_eq!(fuel_needed(14), 2);
-    assert_eq!(fuel_needed(1969), 966);
-    assert_eq!(fuel_needed(100756), 50346);
-  }
+    #[test]
+    fn it_works() {
+        let input = include_str!("./modules.txt");
+
+        let fuel = part_one(input);
+        assert_eq!(fuel, 3317970);
+
+        let fuel = part_two(input);
+        assert_eq!(fuel, 4974073);
+    }
+
+    #[test]
+    fn examples() {
+        assert_eq!(fuel_needed(12), 2);
+        assert_eq!(fuel_needed(14), 2);
+        assert_eq!(fuel_needed(1969), 966);
+        assert_eq!(fuel_needed(100756), 50346);
+    }
 }

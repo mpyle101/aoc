0d3aba3325ca4928dc3cefeb0e54c5fb7c9d57f9
Michael Pyle <mpyle101@gmail.com>
2022-12-25 20:37:26 -0700

    A little tiding

--- 2022/day25/src/main.rs
+++ 2022/day25/src/main.rs
@@ -18,37 +18,35 @@ fn part_one(input: &str) -> String
 
 fn snafu(mut n: i64) -> String
 {
-    use std::char::from_digit;
-
     let mut s = String::new();
-    while let Some(c) = from_digit((n % 5) as u32, 5) {
+    while n > 0 {
+        let c = (n % 5) as u8 + b'0';
         let (c, x) = match c {
-            '4' => ('-', n + 5),
-            '3' => ('=', n + 5),
-             _  => (c, n),
+            b'4' => ('-', n + 5),
+            b'3' => ('=', n + 5),
+              _  => (c as char, n),
         };
         s.insert(0, c);
         n = x / 5;
-        if n == 0 { return s }
     }
 
-    unreachable!()
+    s
 }
 
 fn fubar(s: &str) -> i64
 {
     let (n, _) = s.chars()
         .rev()
-        .fold((0, 1), |(n, p), c| {
+        .fold((0, 0), |(n, p), c| {
             let v = match c {
-                '2' => 2 * p,
-                '1' => p,
-                '0' => 0,
-                '-' => -p,
-                '=' => -2 * p,
+                '2' =>  2,
+                '1' =>  1,
+                '0' =>  0,
+                '-' => -1,
+                '=' => -2,
                  _  => unreachable!()
             };
-            (v + n, p * 5)
+            (n + v * 5i64.pow(p), p + 1)
         });
 
     n
@@ -63,115 +61,53 @@ mod tests {
     fn it_works()
     {
         let input = include_str!("../input.txt");
-
-        let snafu = part_one(input);
-        assert_eq!(snafu, "2-21=02=1-121-2-11-0");
+        assert_eq!(part_one(input), "2-21=02=1-121-2-11-0");
     }
 
     #[test]
     fn example()
     {
         let input = include_str!("../example.txt");
-
-        let snafu = part_one(input);
-        assert_eq!(snafu, "2=-1=0");
+        assert_eq!(part_one(input), "2=-1=0");
     }
 
     #[test]
     fn snafu_works()
     {
-        let s = snafu(1);
-        assert_eq!(s, "1");
-
-        let s = snafu(2);
-        assert_eq!(s, "2");
-
-        let s = snafu(3);
-        assert_eq!(s, "1=");
-
-        let s = snafu(4);
-        assert_eq!(s, "1-");
-
-        let s = snafu(5);
-        assert_eq!(s, "10");
-
-        let s = snafu(6);
-        assert_eq!(s, "11");
-
-        let s = snafu(7);
-        assert_eq!(s, "12");
-
-        let s = snafu(8);
-        assert_eq!(s, "2=");
-
-        let s = snafu(9);
-        assert_eq!(s, "2-");
-
-        let s = snafu(10);
-        assert_eq!(s, "20");
-
-        let s = snafu(15);
-        assert_eq!(s, "1=0");
-
-        let s = snafu(20);
-        assert_eq!(s, "1-0");
-
-        let s = snafu(2022);
-        assert_eq!(s, "1=11-2");
-
-        let s = snafu(4890);
-        assert_eq!(s, "2=-1=0");
-
-        let s = snafu(12345);
-        assert_eq!(s, "1-0---0");
-
-        let s = snafu(314159265);
-        assert_eq!(s, "1121-1110-1=0");
-
-        let s = snafu(35951702021395);
-        assert_eq!(s, "2-21=02=1-121-2-11-0");
+        assert_eq!(snafu(1), "1");
+        assert_eq!(snafu(2), "2");
+        assert_eq!(snafu(3), "1=");
+        assert_eq!(snafu(4), "1-");
+        assert_eq!(snafu(5), "10");
+        assert_eq!(snafu(6), "11");
+        assert_eq!(snafu(7), "12");
+        assert_eq!(snafu(8), "2=");
+        assert_eq!(snafu(9), "2-");
+        assert_eq!(snafu(10), "20");
+        assert_eq!(snafu(15), "1=0");
+        assert_eq!(snafu(20), "1-0");
+        assert_eq!(snafu(2022), "1=11-2");
+        assert_eq!(snafu(4890), "2=-1=0");
+        assert_eq!(snafu(12345), "1-0---0");
+        assert_eq!(snafu(314159265), "1121-1110-1=0");
+        assert_eq!(snafu(35951702021395), "2-21=02=1-121-2-11-0");
     }
 
     #[test]
     fn fubar_works()
     {
-        let n = fubar("1=-0-2");
-        assert_eq!(n, 1747);
-
-        let n = fubar("12111");
-        assert_eq!(n, 906);
-
-        let n = fubar("2=0=");
-        assert_eq!(n, 198);
-
-        let n = fubar("21");
-        assert_eq!(n, 11);
-
-        let n = fubar("2=01");
-        assert_eq!(n, 201);
-
-        let n = fubar("111");
-        assert_eq!(n, 31);
-
-        let n = fubar("20012");
-        assert_eq!(n, 1257);
-
-        let n = fubar("1-12");
-        assert_eq!(n, 107);
-
-        let n = fubar("12");
-        assert_eq!(n, 7);
-
-        let n = fubar("1=");
-        assert_eq!(n, 3);
-
-        let n = fubar("122");
-        assert_eq!(n, 37);
-
-        let n = fubar("2=-01");
-        assert_eq!(n, 976);
-
-        let n = fubar("2-21=02=1-121-2-11-0");
-        assert_eq!(n, 35951702021395);
+        assert_eq!(fubar("1=-0-2"), 1747);
+        assert_eq!(fubar("12111"), 906);
+        assert_eq!(fubar("2=0="), 198);
+        assert_eq!(fubar("21"), 11);
+        assert_eq!(fubar("2=01"), 201);
+        assert_eq!(fubar("111"), 31);
+        assert_eq!(fubar("20012"), 1257);
+        assert_eq!(fubar("1-12"), 107);
+        assert_eq!(fubar("12"), 7);
+        assert_eq!(fubar("1="), 3);
+        assert_eq!(fubar("122"), 37);
+        assert_eq!(fubar("2=-01"), 976);
+        assert_eq!(fubar("2-21=02=1-121-2-11-0"), 35951702021395);
     }
 }

37f26380f320b60e0bede08e5a1dba7e8e6f0b92
Michael Pyle <mpyle101@gmail.com>
2022-12-27 08:46:33 -0700

    A little faster to not make so many strings

--- 2015/day04/src/main.rs
+++ 2015/day04/src/main.rs
@@ -3,20 +3,27 @@ fn main() {
     use std::time::Instant;
 
 	let t = Instant::now();
-    println!("Part 1: {} ({:?})", compute("yzbqklnj", "00000"), t.elapsed());
+    println!("Part 1: {} ({:?})", compute(b"yzbqklnj", 5), t.elapsed());
 
 	let t = Instant::now();
-    println!("Part 2: {} ({:?})", compute("yzbqklnj", "000000"), t.elapsed());
+    println!("Part 2: {} ({:?})", compute(b"yzbqklnj", 6), t.elapsed());
 }
 
-fn compute(secret: &str, tag: &str) -> u32 {
-    let mut n = 0;
+fn compute(secret: &[u8;8], zeros: u8) -> u32 {
+	use md5_rs::Context;
+
+    let mut n = 1;
     loop {
-		let key = secret.to_owned() + &n.to_string();
-		let digest = md5::compute(key);
-		if format!("{:x}", digest).starts_with(tag) {
-			break n
+		let mut ctx = Context::new();
+		ctx.read(secret);
+		ctx.read(n.to_string().as_bytes());
+		let d = ctx.finish();
+
+		// Bytes are 8 bits, hexadecimal values only use 4.
+		if (d[0] | d[1] == 0) && (d[2] == 0 || zeros == 5 && d[2] & 0xF0 == 0) {
+			return n
 		}
+
 		n += 1
     }
 }
@@ -28,11 +35,11 @@ mod tests {
 
 	#[test]
 	fn input_part_one() {
-		assert_eq!(compute("yzbqklnj", "00000"), 282749);
+		assert_eq!(compute(b"yzbqklnj", 5), 282749);
 	}
 
 	#[test]
 	fn input_part_two() {
-		assert_eq!(compute("yzbqklnj", "000000"), 9962624);
+		assert_eq!(compute(b"yzbqklnj", 6), 9962624);
 	}
 }

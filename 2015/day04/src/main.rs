b9803c8904a061174b355f11b3d996a0f3e802a1
Michael Pyle <mpyle101@gmail.com>
2022-12-26 23:03:01 -0700

    Futzing & formatting

--- 2015/day04/src/main.rs
+++ 2015/day04/src/main.rs
@@ -1,34 +1,38 @@
-fn main() {
-    let hash = compute("yzbqklnj", "00000");
-    println!("Part 1: {}", hash);
 
-    let hash = compute("yzbqklnj", "000000");
-    println!("Part 2: {}", hash);
+fn main() {
+    use std::time::Instant;
+
+	let t = Instant::now();
+    println!("Part 1: {} ({:?})", compute("yzbqklnj", "00000"), t.elapsed());
+
+	let t = Instant::now();
+    println!("Part 2: {} ({:?})", compute("yzbqklnj", "000000"), t.elapsed());
 }
 
 fn compute(secret: &str, tag: &str) -> u32 {
     let mut n = 0;
     loop {
-        let key = secret.to_owned() + &n.to_string();
-        let digest = md5::compute(key);
-        if format!("{:x}", digest).starts_with(tag) {
-            break n
-        }
-        n += 1
+		let key = secret.to_owned() + &n.to_string();
+		let digest = md5::compute(key);
+		if format!("{:x}", digest).starts_with(tag) {
+			break n
+		}
+		n += 1
     }
 }
 
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+	use super::*;
 
-  #[test]
-  fn it_works() {
-    let hash = compute("yzbqklnj", "00000");
-    assert_eq!(hash, 282749);
+	#[test]
+	fn input_part_one() {
+		assert_eq!(compute("yzbqklnj", "00000"), 282749);
+	}
 
-    let hash = compute("yzbqklnj", "000000");
-    assert_eq!(hash, 9962624);
-  }
+	#[test]
+	fn input_part_two() {
+		assert_eq!(compute("yzbqklnj", "000000"), 9962624);
+	}
 }

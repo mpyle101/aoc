e335980104d1e7543deadcb40963bbc6e6c64a2a
Michael Pyle <mpyle101@gmail.com>
2023-01-16 12:06:44 -0700

    A little over 10x faster for part two

--- 2016/day11/src/main.rs
+++ 2016/day11/src/main.rs
@@ -1,11 +1,6 @@
-use lazy_static::lazy_static;
 
-lazy_static! {
-    // Cobalt, Polonium, Promethium, Ruthenium, Thulium
-    static ref ISOTOPES: [usize; 5] = [1, 3, 5, 7, 9];
-}
-
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
     // Didn't feel like parsing the input (really? sentences?).
@@ -17,21 +12,19 @@ fn main() {
     // in the state for a given isotope's floor values. From
     // this, the goal state consists of an array of all 4's.
 
-    let t1 = Instant::now();
-    let steps = part_one();
-    let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", steps, t2 - t1);
+    let t = Instant::now();
+    println!("Part 1: {} ({:?})", part_one(), t.elapsed());
 
-    let t1 = Instant::now();
-    let steps = part_two();
-    let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", steps, t2 - t1);
+    let t = Instant::now();
+    println!("Part 2: {} ({:?})", part_two(), t.elapsed());
 }
 
-fn part_one() -> usize {
+fn part_one() -> usize
+{
     use pathfinding::prelude::bfs;
 
     let state = [1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1];
+    // Cobalt, Polonium, Promethium, Ruthenium, Thulium
     let isotopes = [1, 3, 5, 7, 9];
 
     let goal = [4u8;11];
@@ -43,11 +36,14 @@ fn part_one() -> usize {
     v.len() - 1
 }
 
-fn part_two() -> usize {
+#[allow(dead_code)]
+fn part_two() -> usize
+{
     use pathfinding::prelude::bfs;
 
     let state = [1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-    let isotopes = [1, 3, 5, 7, 9];
+    // Cobalt, Polonium, Promethium, Ruthenium, Thulium, Elerium, Dilithium
+    let isotopes = [1, 3, 5, 7, 9, 11, 13];
 
     let goal = [4u8;15];
     let steps = bfs(&state, |st| next_states(st, &isotopes), |&st| st == goal);
@@ -58,70 +54,76 @@ fn part_two() -> usize {
     v.len() - 1
 }
 
-fn next_states<const N: usize>(state: &[u8;N], isotopes: &[usize]) -> Vec<[u8;N]> {
+fn next_states<const N: usize>(state: &[u8;N], isotopes: &[usize]) -> Vec<[u8;N]>
+{
     // Get the floors the elevator can move to.
     let elevator = state[0];
-    let floors = match elevator {
-        1 => vec![2],
-        2 => vec![1, 3],
-        3 => vec![2, 4],
-        4 => vec![3],
-        _ => panic!("How did we get here?!?! {}", elevator)
+    let floors: u8 = match elevator {
+        1 => 0b0010,
+        2 => 0b0101,
+        3 => 0b1010,
+        4 => 0b0100,
+        _ => unreachable!()
     };
 
     // Get the objects on the current floor (skip the elevator).
-    let objects = state.iter().enumerate().skip(1)
+    let objects = state.iter()
+        .enumerate()
+        .skip(1)
         .filter_map(|(i, &n)| (n == elevator).then_some(i))
         .collect::<Vec<_>>();
 
     // Get all possible states of moving one or two objects
     // to the available floors and filter out the ones with
     // unprotected microchips on the same floor as generators.
-    get_all(state, &objects, &floors)
-        .iter()
-        .filter_map(|st| invalid(st, isotopes))
+    get_all(state, &objects, floors)
+        .filter(|st| valid(st, isotopes))
         .collect()
 }
 
-fn get_all<const N: usize>(state: &[u8;N], objects: &[usize], floors: &[u8]) -> Vec<[u8;N]> {
+fn get_all<'a, const N: usize>(
+    state: &'a [u8;N],
+    objects: &'a [usize],
+    floors: u8) -> impl Iterator<Item = [u8;N]> + 'a
+{
+    use bit_iter::BitIter;
     use itertools::Itertools;
 
-    let states = floors.iter().flat_map(|&floor| {
-        let mut states = objects.iter().map(|&i| {
-            let mut st = *state;
-            st[0] = floor;
-            st[i] = floor;
-            st
-        })
-        .collect::<Vec<_>>();
-
-        objects.iter().combinations(2).for_each(|v| {
-            let mut st = *state;
-            st[0] = floor;
-            st[*v[0]] = floor;
-            st[*v[1]] = floor;
-            states.push(st)
-        });
-
-        states
+    let iter = BitIter::from(floors);
+    iter.flat_map(move |b| {
+        let floor = b as u8 + 1;
+        objects.iter()
+            .map(move |&i| {
+                let mut st = *state;
+                st[0] = floor;
+                st[i] = floor;
+                st
+            })
+        .chain(objects.iter()
+            .combinations(2)
+            .map(move |v| {
+                let mut st = *state;
+                st[0] = floor;
+                st[*v[0]] = floor;
+                st[*v[1]] = floor;
+                st
+            })
+        )
     })
-    .collect();
-
-    states
 }
 
-fn invalid<const N: usize>(state: &[u8;N], isotopes: &[usize]) -> Option<[u8;N]> {
+fn valid<const N: usize>(state: &[u8;N], isotopes: &[usize]) -> bool
+{
     // A state is invalid if there are unprotected microchips
     // on the same floor as a generator for another isotope.
-    let found = isotopes.iter()
+    !isotopes.iter()
         .filter(|&i| state[*i] != state[i + 1])
-        .any(|&i| isotopes.iter().any(|n| state[i] == state[n + 1]));
-
-    (!found).then_some(*state)
+        .any(|&i| isotopes.iter().any(|n| state[i] == state[n + 1]))
 }
 
 #[allow(dead_code)]
-fn print(state: &[u8]) {
+fn print(state: &[u8])
+{
     let symbols = [
         "E ",
         "Cm", "Cg",
@@ -154,12 +156,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let steps = part_one();
-        assert_eq!(steps, 47);
+    fn input_part_one()
+    {
+        assert_eq!(part_one(), 47);
+    }
 
-        // slow, ~100 seconds in release
-        let steps = part_two();
-        assert_eq!(steps, 71);
+    #[test]
+    fn input_part_two()
+    {
+        assert_eq!(part_two(), 71);
     }
 }

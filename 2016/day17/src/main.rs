6979465b5db1e091321309a9a1ca7e9295cc160f
Michael Pyle <mpyle101@gmail.com>
2023-01-21 21:06:18 -0700

    About 3x faster not using strings

--- 2016/day17/src/main.rs
+++ 2016/day17/src/main.rs
@@ -1,46 +1,38 @@
-use lazy_static::lazy_static;
 
-lazy_static! {
-    static ref DELTA: [((i32, i32), char);4] = [
-        (( 0, -1), 'U'),
-        (( 0,  1), 'D'),
-        ((-1,  0), 'L'),
-        (( 1,  0), 'R')
-    ];
-}
-
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
-    let t1 = Instant::now();
-    let path = part_one("veumntbg");
-    let t2 = Instant::now();
-    println!("Part 1: {path} ({:?})", t2 - t1);
+    let t = Instant::now();
+    println!("Part 1: {} ({:?})", part_one("veumntbg"), t.elapsed());
 
-    let t1 = Instant::now();
-    let steps = part_two("veumntbg");
-    let t2 = Instant::now();
-    println!("Part 2: {steps} ({:?})", t2 - t1);
+    let t = Instant::now();
+    println!("Part 2: {} ({:?})", part_two("veumntbg"), t.elapsed());
 }
 
-type State = ((i32, i32), String);
+type State = ((i8, i8), Vec<u8>);
 
-fn part_one(passcode: &str) -> String {
+fn part_one(input: &str) -> String
+{
     use pathfinding::prelude::bfs;
     
     let goal = (3, 3);
-    let steps = bfs(&((0, 0), passcode.to_string()), doors, |st| st.0 == goal);
-    let s = steps.unwrap().last().unwrap().1.clone();
+    let passcode = input.bytes().collect::<Vec<_>>();
+    let steps = bfs(&((0, 0), passcode), doors, |st| st.0 == goal).unwrap();
+    let (_, v) = steps.last().unwrap();
     
-    s[passcode.len()..].to_string()
+    v[input.len()..].iter().map(|&b| b as char).collect::<String>()
 }
 
-fn part_two(passcode: &str) -> usize {
+#[allow(dead_code)]
+fn part_two(input: &str) -> usize
+{
     use pathfinding::prelude::yen;
     
     let goal = (3, 3);
-    let steps: Vec<(Vec<State>, usize)> = yen(
-        &((0, 0), passcode.to_string()),
+    let passcode = input.bytes().collect::<Vec<_>>();
+    let steps = yen(
+        &((0, 0), passcode),
         |st| doors(st).iter().map(|st| (st.clone(), 1)).collect::<Vec<_>>(),
         |st| st.0 == goal,
         1500    // manually increase until longest path doesn't change
@@ -48,43 +40,64 @@ fn part_two(passcode: &str) -> usize {
 
     let mut paths = steps.iter().map(|v| v.0.last().unwrap().1.clone()).collect::<Vec<_>>();
     paths.sort_by_key(|a| a.len());
-    let longest = paths.last().unwrap().len() - passcode.len();
+    let longest = paths.last().unwrap().len() - input.len();
 
     longest
 }
 
-fn doors(((x, y), passcode): &State) -> Vec<State> {
-    let hash  = format!("{:x}", md5::compute(passcode));
-    let doors = hash[0..4].chars().map(|c| c as u8).collect::<Vec<_>>();
+const DELTA: [((i8, i8), u8);4] = [
+    (( 0, -1), b'U'),
+    (( 0,  1), b'D'),
+    ((-1,  0), b'L'),
+    (( 1,  0), b'R')
+];
 
-    DELTA.iter().enumerate()
-        .filter_map(|(i, ((dx, dy), c))| {
-            let d = doors[i] - b'a';
-            if d > 0 && d < b'g' {
-                let pt = (x + dx, y + dy);
-                if pt.0 >= 0 && pt.0 < 4 && pt.1 >= 0 && pt.1 < 4 {
-                    Some((pt, format!("{}{}", passcode, c)))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
+fn doors(((x, y), passcode): &State) -> Vec<State>
+{
+    let h = md5::compute(passcode);
+    [
+        h[0] >> 4 & 0x0F,
+        h[0] & 0x0F,
+        h[1] >> 4 & 0x0F,
+        h[1] & 0x0F
+    ].iter()
+        .enumerate()
+        .filter_map(move |(i, door)| {
+            let ((dx, dy), c) = DELTA[i];
+            let pt = (x + dx, y + dy);
+            (is_open(door) && in_bounds(pt)).then_some({
+                let mut v = passcode.clone();
+                v.push(c);
+                (pt, v)
+            })
         })
         .collect()
 }
 
+fn in_bounds((x, y): (i8, i8)) -> bool
+{
+    (0..4).contains(&x) && (0..4).contains(&y)
+}
+
+fn is_open(b: &u8) -> bool
+{
+    // hex b, c, d, e & f
+    (11..16).contains(b)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let path = part_one("veumntbg");
-        assert_eq!(path, "DDRRULRDRD");
-        
-        let steps = part_two("veumntbg");
-        assert_eq!(steps, 536);
+    fn input_part_one()
+    {
+        assert_eq!(part_one("veumntbg"), "DDRRULRDRD");
+    }
+
+    #[test]
+    fn input_part_two()
+    {
+        assert_eq!(part_two("veumntbg"), 536);
     }
 }

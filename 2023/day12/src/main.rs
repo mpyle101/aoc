d79a3436fd9d8b09a44f917795b8338e8c0dcf1b
Michael Pyle <mpyle101@gmail.com>
2023-12-17 13:39:54 -0700

    Day 12, Part 1

--- 2023/day12/src/main.rs
+++ 2023/day12/src/main.rs
@@ -2,7 +2,7 @@ fn main()
 {
     use std::time::Instant;
 
-    let input = include_str!("../example1.txt");
+    let input = include_str!("../input.txt");
 
     let t = Instant::now();
     let result = part_one(input);
@@ -34,9 +34,8 @@ fn part_two(input: &str) -> u32
                 .collect::<Vec<_>>()
                 .join("?");
             let groups = (0..5)
-                .map(|_| g.clone())
-                .collect::<Vec<_>>()
-                .join(".");
+                .flat_map(|_| g.clone())
+                .collect::<Vec<_>>();
 
             arrangements(&springs, &groups)
         })
@@ -44,7 +43,7 @@ fn part_two(input: &str) -> u32
 }
 
 
-fn parse_record(line: &str) -> (String, String)
+fn parse_record(line: &str) -> (String, Vec<u32>)
 {
     let (s, g) = line.split_once(' ').unwrap();
     let springs = s.split('.')
@@ -53,107 +52,56 @@ fn parse_record(line: &str) -> (String, String)
         .join(".");
     let groups = g.split(',')
         .flat_map(|n| n.parse())
-        .map(|n| "#".repeat(n))
-        .collect::<Vec<_>>()
-        .join(".");
+        .collect();
 
     (springs, groups)
 }
 
-fn arrangements(springs: &str, groups: &str) -> u32
+fn arrangements(springs: &str, groups: &[u32]) -> u32
 {
-    let sp = springs.as_bytes();
-    let gp = groups.as_bytes();
-
-    //count_2d(sp, gp)
-    count_arr(sp, gp)
+    count(springs, groups, &[0])
 }
 
-#[allow(dead_code)]
-fn count_arr(sp: &[u8], gp: &[u8]) -> u32
+fn count(springs: &str, groups: &[u32], found: &[u32]) -> u32
 {
-    if let (Ok(s), Ok(g)) = (std::str::from_utf8(sp), std::str::from_utf8(gp)) {
-        println!("{s}  {g}");
-    }
-    
-    // Due to stripping, every springs sequence starts with
-    // '?' or '#' so we know the first character is always a
-    // match.
-    let mut dp = vec![0;gp.len()+1];
-    dp[0] = 1;
+    let i = found.len() - 1;
 
-    for b in sp.iter().skip(1) {
-        for j in (1..=gp.len()).rev() {
-            if *b == gp[j-1] || *b == b'?' {
-                dp[j] += dp[j-1]
-            }
+    if springs.is_empty() {
+        if found[i] == 0 {
+            return (groups == &found[..i]) as u32
+        } else {
+            return (groups == found) as u32
         }
     }
 
-    println!("{:?}", dp);
+    let c = springs.chars().next().unwrap();
     
-    dp[dp.len() - 1]
-}
-
-#[allow(dead_code)]
-fn count_2d(sp: &[u8], gp: &[u8]) -> u32
-{
-    if let (Ok(s), Ok(g)) = (std::str::from_utf8(sp), std::str::from_utf8(gp)) {
-        println!("{s}  {g}");
-    }
-
-    let mut dp = (0..=gp.len()).map(|_| vec![0;sp.len()+1]).collect::<Vec<_>>();
-    
-    // Due to stripping, every springs sequence starts with
-    // '?' or '#' so we know the first character is always a
-    // match.
-    dp[0][0] = 1;
-
-    for i in 1..=gp.len() {
-        for j in 1..=sp.len() {
-            dp[i][j] = dp[i][j-1];
-            if gp[i-1] == sp[j-1] || sp[j-1] == b'?' {
-                dp[i][j] += dp[i-1][j-1];
-            }
+    let mut n = 0;
+    if c == '.' {
+        if found[i] == 0 {
+            n += count(&springs[1..], groups, found)
+        } else {
+            let mut v = found.to_vec(); v.push(0);
+            n += count(&springs[1..], groups, &v)
         }
-    }
-
-    dp.iter().for_each(|v| println!("{:?}", v));
-    
-    dp[gp.len()][sp.len()]
-}
-
-#[allow(dead_code)]
-fn count_recur(sp: &[u8], gp: &[u8]) -> u32
-{
-    use std::str::from_utf8;
-
-    if let (Ok(s), Ok(g)) = (from_utf8(sp), from_utf8(gp)) {
-        print!("{s}  {g} => ");
-    }
-    let count = if gp.is_empty() {
-        sp.iter().all(|c| *c == b'?') as u32
-    } else if gp.len() > sp.len() {
-        0
-    } else if gp.len() == sp.len() && (0..gp.len()).all(|i| gp[i] == sp[i] || sp[i] == b'?') {
-        1
-    } else if sp[0] == gp[0] || sp[0] == b'?' {
-        let i = if gp[0] == b'.' && sp[0] == b'?' { 1 + (sp[1] == b'.') as usize } else { 1 };
-        if let (Ok(s), Ok(g)) = (from_utf8(&sp[i..]), from_utf8(&gp[1..])) {
-            print!("({s}  {g}), ");
-        }
-        if let (Ok(s), Ok(g)) = (from_utf8(&sp[i..]), from_utf8(gp)) {
-            println!("({s}  {g})");
-        }
-        count_recur(&sp[i..], &gp[1..]) + count_recur(&sp[i..], gp)
+    } else if c == '#' {
+        let mut v = found.to_vec(); v[i] += 1;
+        n += count(&springs[1..], groups, &v)
     } else {
-        0
-    };
-
-    if let (Ok(s), Ok(g)) = (from_utf8(sp), from_utf8(gp)) {
-        println!("{s}  {g} => {count}");
+        // as '#'
+        let mut v = found.to_vec(); v[i] += 1;
+        n += count(&springs[1..], groups, &v);
+ 
+        // as '.'
+        if found[i] == 0 {
+            n += count(&springs[1..], groups, found)
+        } else {
+            let mut v = found.to_vec(); v.push(0);
+            n += count(&springs[1..], groups, &v)
+        }
     }
-    count
+
+    n
 }
 
 
@@ -178,36 +126,36 @@ mod tests {
     #[test]
     fn example_1()
     {
-        assert_eq!(arrangements("???.###", "#.#.###"), 1);
+        assert_eq!(arrangements("???.###", &[1,1,3]), 1);
     }
 
     #[test]
     fn example_2()
     {
-        assert_eq!(arrangements("??.??.?##", "#.#.###"), 4);
+        assert_eq!(arrangements("??.??.?##", &[1,1,3]), 4);
     }
 
     #[test]
     fn example_3()
     {
-        assert_eq!(arrangements("?#?#?#?#?#?#?#?", "#.###.#.######"), 1);
+        assert_eq!(arrangements("?#?#?#?#?#?#?#?", &[1,3,1,6]), 1);
     }
 
     #[test]
     fn example_4()
     {
-        assert_eq!(arrangements("????.#.#", "####.#.#"), 1);
+        assert_eq!(arrangements("????.#.#", &[4,1,1]), 1);
     }
 
     #[test]
     fn example_5()
     {
-        assert_eq!(arrangements("????.######.#####", "#.######.#####"), 4);
+        assert_eq!(arrangements("????.######.#####", &[1,6,5]), 4);
     }
 
     #[test]
     fn example_6()
     {
-        assert_eq!(arrangements("?###????????", "###.##.#"), 10);
+        assert_eq!(arrangements("?###????????", &[3,2,1]), 10);
     }
 }

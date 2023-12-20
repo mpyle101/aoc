d294e39e7517180e6be57d5f40fe5d5d0839c438
Michael Pyle <mpyle101@gmail.com>
2023-12-19 21:57:56 -0700

    Day 19, Part 2

--- 2023/day19/src/main.rs
+++ 2023/day19/src/main.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 
+type Workflows<'a> = HashMap<&'a str, Vec<Rule>>;
+type Ratings = [(u64, u64);4];
+type State<'a> = (&'a str, Ratings);
+
 fn main()
 {
     use std::time::Instant;
@@ -15,38 +19,91 @@ fn main()
     println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(input: &str) -> u32
+fn part_one(input: &str) -> u64
 {
-    let mut iter = input.split("\n\n");
-    let wfs = load_workflows(iter.next().unwrap());
-    let parts = load_parts(iter.next().unwrap());
+    let (s1, s2) = input.split_once("\n\n").unwrap();
+    let wfs = load_workflows(s1);
+    let parts = load_parts(s2);
 
     parts.iter()
         .filter(|part| run(part, &wfs) == "A")
-        .map(|part| part.iter().sum::<u32>())
+        .map(|part| part.iter().sum::<u64>())
         .sum()
 }
 
 fn part_two(input: &str) -> u64
 {
-    let mut iter = input.split("\n\n");
-    let wfs = load_workflows(iter.next().unwrap());
-    let parts = load_parts(iter.next().unwrap());
+    use std::collections::VecDeque;
 
-    parts.iter()
-        .filter(|part| run(part, &wfs) == "A")
-        .map(|part| part.iter().sum::<u32>() as u64)
-        .sum::<u64>()
+    let (s1, _) = input.split_once("\n\n").unwrap();
+    let wfs = load_workflows(s1);
+
+    let mut accepted = vec![];
+
+    let mut q = VecDeque::from([("in", [(1u64, 4000u64);4])]);
+    while let Some((wf, parts)) = q.pop_front() {
+        let mut ratings = parts;
+        for rule in wfs.get(wf).unwrap() {
+            let (st, rat) = process(&ratings, rule);
+            if st.0 == "A" {
+                accepted.push(st.1);
+            } else if st.0 != "R" {
+                q.push_back(st);
+            }
+            ratings = rat;
+        }
+    }
+
+    //accepted.iter().for_each(|a| println!("{:?}", a));
+    
+    accepted.iter()
+        .map(|ratings| ratings.iter()
+            .filter(|(a, b)| a <= b)
+            .map(|(a, b)| b - a + 1)
+            .product::<u64>()
+        )
+        .sum()
 }
 
+fn process<'a>(ratings: &Ratings, rule: &'a Rule) -> (State<'a>, Ratings)
+{
+    match rule.op {
+        '=' => ((&rule.wf, *ratings), *ratings),
+        '>' => {
+            let mut rat = *ratings;
+            let rng = rat[rule.rat];
+            rat[rule.rat] = (rng.0.max(rule.val + 1), rng.1);
+            let st: State = (&rule.wf, rat);
+
+            let mut rat = *ratings;
+            let rng = rat[rule.rat];
+            rat[rule.rat] = (rng.0, rng.1.min(rule.val));
+            (st, rat)
+        },
+        '<' => {
+            let mut rat = *ratings;
+            let rng = rat[rule.rat];
+            rat[rule.rat] = (rng.0, rng.1.min(rule.val - 1));
+            let st: State = (&rule.wf, rat);
+
+            let mut rat = *ratings;
+            let rng = rat[rule.rat];
+            rat[rule.rat] = (rng.0.max(rule.val), rng.1);
+            (st, rat)
+        },
+        _ => panic!("Unknown operation: {}", rule.op)
+    }
+}
+
+#[derive(Debug)]
 struct Rule {
     op: char,
     wf: String,
-    val: u32,
-    cat: usize,
+    val: u64,
+    rat: usize,
 }
 
-fn run<'a>(part: &[u32;4], wfs: &'a HashMap<&str, Vec<Rule>>) -> &'a str
+fn run<'a>(part: &[u64;4], wfs: &'a Workflows) -> &'a str
 {
     let mut name = "in";
 
@@ -56,8 +113,8 @@ fn run<'a>(part: &[u32;4], wfs: &'a HashMap<&str, Vec<Rule>>) -> &'a str
             while iter.next_if(|rule|
                 match rule.op {
                     '=' => false,
-                    '<' => part[rule.cat] >= rule.val,
-                    '>' => part[rule.cat] <= rule.val,
+                    '<' => part[rule.rat] >= rule.val,
+                    '>' => part[rule.rat] <= rule.val,
                     _ => panic!("Unknown operation: {}", rule.op)
                 }).is_some() {}
             name = &iter.next().unwrap().wf;
@@ -81,17 +138,17 @@ fn load_workflows(wf: &str) -> HashMap<&str, Vec<Rule>>
             let (name, s) = line.split_once('{').unwrap();
             let mut rules = rules_re.captures_iter(s)
                 .map(|caps| {
-                    let cat = category(&caps[1]);
+                    let rat = category(&caps[1]);
                     let op  = caps[2].chars().next().unwrap();
-                    let val = caps[3].parse::<u32>().unwrap();
+                    let val = caps[3].parse::<u64>().unwrap();
                     let wf  = caps[4].to_string();
 
-                    Rule { wf, cat, op, val }
+                    Rule { wf, rat, op, val }
                 })
                 .collect::<Vec<_>>();
             if let Some(caps) = last_re.captures(s) {
                 let wf = caps[1].to_string();
-                rules.push(Rule { wf, cat: 5, op: '=', val: 0 })
+                rules.push(Rule { wf, rat: 5, op: '=', val: 0 })
             } else {
                 panic!("Default rule not found!")
             }
@@ -112,7 +169,7 @@ fn category(c: &str) -> usize
     }
 }
 
-fn load_parts(parts: &str) -> Vec<[u32;4]>
+fn load_parts(parts: &str) -> Vec<[u64;4]>
 {
     parts.lines()
         .map(|line| {
@@ -122,7 +179,7 @@ fn load_parts(parts: &str) -> Vec<[u32;4]>
                 .enumerate()
                 .for_each(|(i, c)| {
                     let (_, n) = c.split_once('=').unwrap();
-                    xmas[i] = n.parse::<u32>().unwrap();
+                    xmas[i] = n.parse::<u64>().unwrap();
                 });
             xmas
         })
@@ -144,7 +201,7 @@ mod tests {
     fn input_part_two()
     {
         let input = include_str!("../input.txt");
-        assert_eq!(part_two(input), 362930);
+        assert_eq!(part_two(input), 116365820987729);
     }
 
     #[test]
@@ -158,6 +215,6 @@ mod tests {
     fn example_part_two()
     {
         let input = include_str!("../example.txt");
-        assert_eq!(part_two(input), 167409079868000);
+        assert_eq!(part_two(input), 167_409_079_868_000);
     }
 }

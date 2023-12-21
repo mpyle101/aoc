1b305c7714207fd25e1112d36023b7d5d3dcb9fa
Michael Pyle <mpyle101@gmail.com>
2023-12-20 23:31:02 -0700

    Day 18, Part 2

---
+++ 2023/day18/src/main.rs
@@ -0,0 +1,200 @@
+fn main()
+{
+    use std::time::Instant;
+
+    let input = include_str!("../input.txt");
+
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
+}
+
+fn part_one(input: &str) -> i64
+{
+    let mut top_lt = (i64::MAX, i64::MAX);
+    let mut bot_rt = (i64::MIN, i64::MIN);
+
+    let mut p = (0, 0);
+    let path: Vec<_> = input.lines()
+        .fold(vec![(0 ,0)], |mut v, line| {
+            let mut iter = line.split(' ');
+            let dir = iter.next().unwrap().chars().next().unwrap();
+            let n = iter.next().map(|s| s.parse().unwrap()).unwrap();
+            p = dig(p, dir, n);
+
+            top_lt.0 = top_lt.0.min(p.0);
+            top_lt.1 = top_lt.1.min(p.1);
+            bot_rt.0 = bot_rt.0.max(p.0);
+            bot_rt.1 = bot_rt.1.max(p.1);
+    
+            v.push(p);
+            v
+        });
+
+    fill(top_lt, bot_rt, &path)
+}
+
+#[allow(dead_code)]
+fn part_two(input: &str) -> i64
+{
+    let mut top_lt = (i64::MAX, i64::MAX);
+    let mut bot_rt = (i64::MIN, i64::MIN);
+
+    let mut p = (0, 0);
+    let path: Vec<_> = input.lines()
+        .fold(vec![(0 ,0)], |mut v, line| {
+            let mut iter = line.split(' ');
+            iter.next();    // dir
+            iter.next();    // n
+            let mut code = iter.next().unwrap();
+            code = &code[2..code.len()-1];
+            let n = i64::from_str_radix(&code[0..5], 16).unwrap();
+            let dir = match code.as_bytes()[5] {
+                b'0' => 'R',
+                b'1' => 'D',
+                b'2' => 'L',
+                b'3' => 'U',
+                _ => panic!("Unkwown direction: {code}")
+            };
+            p = dig(p, dir, n);
+
+            top_lt.0 = top_lt.0.min(p.0);
+            top_lt.1 = top_lt.1.min(p.1);
+            bot_rt.0 = bot_rt.0.max(p.0);
+            bot_rt.1 = bot_rt.1.max(p.1);
+    
+            v.push(p);
+            v
+        });
+
+    fill(top_lt, bot_rt, &path)
+}
+
+fn dig((x, y): (i64, i64), dir: char, n: i64) -> (i64, i64)
+{
+    if dir == 'U' {
+        (x, y - n)
+    } else if dir == 'D' {
+        (x, y + n)
+    } else if dir == 'L' {
+        (x - n, y)
+    } else {
+        (x + n, y)
+    }
+}
+
+fn fill(
+    _tl: (i64, i64),
+    _br: (i64, i64),
+    path: &[(i64, i64)]
+) -> i64
+{
+    let mut h_edges: Vec<_> = path.windows(2)
+        .map(|ch| (ch[0], ch[1]))
+        .filter(|(p1, p2)| p1.1 == p2.1)
+        .map(|(p1, p2)| if p1.0 < p2.0 { (p1, p2) } else { (p2, p1) })
+        .collect();
+    h_edges.sort_by(|p1, p2| p1.1.1.cmp(&p2.1.1));
+    let mut h_lines: Vec<_> = h_edges.iter()
+        .map(|(p1, _)| p1.1)
+        .collect();
+    h_lines.sort();
+    h_lines.dedup();
+
+    let mut v_edges: Vec<_> = path.windows(2)
+        .map(|ch| (ch[0], ch[1]))
+        .filter(|(p1, p2)| p1.1 != p2.1)
+        .map(|(p1, p2)| if p1.1 < p2.1 { (p1, p2) } else { (p2, p1) })
+        .collect();
+    v_edges.sort_by(|p1, p2| p1.0.0.cmp(&p2.0.0));
+    let mut v_lines: Vec<_> = v_edges.iter()
+        .map(|(p1, _)| p1.0)
+        .collect();
+    v_lines.sort();
+    v_lines.dedup();
+
+    let rects: Vec<_> = v_lines.windows(2)
+        .map(|v| (v[0], v[1]))
+        .flat_map(|(x_min, x_max)| {
+            let edges: Vec<_> = h_edges.iter()
+                .filter(|e| e.0.0 <= x_min && e.1.0 >= x_max)
+                .collect();
+            edges.chunks(2)
+                .map(|e| (e[0], e[1]))
+                .map(|(e1, e2)| ((x_min, e1.0.1), (x_max, e2.0.1)))
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let mut area = rects.iter()
+        .map(|(tl, br)| (br.0 - tl.0 + 1) * (br.1 - tl.1 + 1))
+        .sum();
+
+    for i in 0..rects.len() - 1 {
+        let a = rects[i];
+        for b in rects.iter().skip(i+1) {
+            if let Some(r) = intersection(a, *b) {
+                area -= (r.1.0 - r.0.0 + 1) * (r.1.1 - r.0.1 + 1)
+            }
+        }
+    }
+
+    area
+}
+
+type Rect = ((i64, i64), (i64, i64));
+
+fn intersection(
+    ((x1, y1), (x2, y2)): Rect,
+    ((x3, y3), (x4, y4)): Rect
+) -> Option<Rect>
+{
+    let x5 = x1.max(x3);
+    let x6 = x2.min(x4);
+    let y5 = y1.max(y3);
+    let y6 = y2.min(y4);
+
+    if x5 <= x6 && y5 <= y6 {
+        Some(((x5, y5), (x6, y6)))
+    } else {
+        None
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 62573);
+    }
+
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 54662804037719);
+    }
+
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one(input), 62);
+    }
+
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_two(input), 952_408_144_115);
+    }
+}

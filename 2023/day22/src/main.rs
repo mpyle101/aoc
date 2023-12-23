659609f67b3596eef82a5849d11b19fdeea710f9
Michael Pyle <mpyle101@gmail.com>
2023-12-22 21:41:28 -0700

    Day 22, Part 2

--- 2023/day22/src/main.rs
+++ 2023/day22/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 type Brick = [[u32;3];2];
+type Bricks = HashMap<Brick, Vec<Brick>>;
 
 fn main()
 {
@@ -9,12 +12,14 @@ fn main()
     let t = Instant::now();
     let result = part_one(input);
     println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
 fn part_one(input: &str) -> u32
 {
-    use std::collections::HashMap;
-
     let mut bricks: Vec<_> = input.lines()
         .map(|line| {
             let (l, r) = line.split_once('~').unwrap();
@@ -28,15 +33,14 @@ fn part_one(input: &str) -> u32
     // Sort into ascending z order.
     bricks.sort_by(|a, b| a[0][2].cmp(&b[0][2]));
 
-    // Map of brick to minimum group of bricks supporting
-    // other bricks.
     let mut supports: HashMap<_,_> = bricks.iter()
         .filter(|brick| brick[0][2] == 1)
         .map(|brick| (*brick, u32::MAX))
         .collect();
 
     // Settle the bricks from the bottom up and
-    // resort into z order.
+    // keep track of the minimum size of any set
+    // of support bricks the brick is in.
     for i in 0..bricks.len() {
         let (brick, supported_by) = fall(&bricks[i], &bricks);
         bricks[i] = brick;
@@ -44,12 +48,48 @@ fn part_one(input: &str) -> u32
 
         let n = supported_by.len() as u32;
         supported_by.iter()
-            .for_each(|brick| {
-                supports.entry(*brick).and_modify(|b| *b = (*b).min(n));
+            .for_each(|b| {
+                supports.entry(*b).and_modify(|e| *e = (*e).min(n));
             })
     }
 
     supports.values().filter(|n| **n > 1).count() as u32
+}
+
+fn part_two(input: &str) -> u32
+{
+    let mut bricks: Vec<_> = input.lines()
+        .map(|line| {
+            let (l, r) = line.split_once('~').unwrap();
+            let p1 = parse_coords(l);
+            let p2 = parse_coords(r);
+
+            [p1, p2]
+        })
+        .collect();
+
+    // Sort into ascending z order.
+    bricks.sort_by(|a, b| a[0][2].cmp(&b[0][2]));
+
+    // Settle the bricks from the bottom up and
+    // rand track which bricks support and are
+    // supported by others.
+    let mut supports = Bricks::new();
+    let mut supported_by = Bricks::new();
+    for i in 0..bricks.len() {
+        let (brick, v) = fall(&bricks[i], &bricks);
+        bricks[i] = brick;
+        supported_by.insert(brick, v.clone());
+
+        v.iter()
+            .for_each(|b| {
+                supports.entry(*b).or_default().push(brick);
+            })
+    }
+
+    bricks.iter()
+        .map(|brick| disintegrate(brick, &supports, &supported_by))
+        .sum()
 }
 
 fn parse_coords(s: &str) -> [u32;3]
@@ -95,14 +135,30 @@ fn intersect(a: &Brick, b: &Brick) -> bool
     a[1][1] >= b[0][1]      // a.max& >= b.minY
 }
 
-#[allow(dead_code)]
-fn intersection(a: &Brick, b: &Brick) -> u32
+fn disintegrate(brick: &Brick, supports: &Bricks, supported_by: &Bricks) -> u32
 {
-    use std::cmp::{min, max};
+    use std::collections::VecDeque;
 
-    max(min(a[1][0], b[1][0]) - max(a[0][0], b[0][0]), 0)
-        + max(min(a[1][1], b[1][1]) - max(a[0][1], b[0][1]), 0)
-        + max(min(a[1][2], b[1][2]) - max(a[0][2], b[0][2]), 0)
+    let mut m = supported_by.clone();
+    let mut q = VecDeque::from([brick]);
+
+    let mut count = 0;
+    while let Some(b1) = q.pop_front() {
+        if let Some(v1) = supports.get(b1) {
+            v1.iter()
+                .for_each(|b2| {
+                    let v2 = m.get_mut(b2).unwrap();
+                    let p = v2.iter().position(|b| b == b1).unwrap();
+                    v2.remove(p);
+                    if v2.is_empty() {
+                        count += 1;
+                        q.push_back(b2);
+                    }
+                })
+        }
+    }
+
+    count
 }
 
 
@@ -118,9 +174,23 @@ mod tests {
     }
 
     #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 89727);
+    }
+
+    #[test]
     fn example_part_one()
     {
         let input = include_str!("../example.txt");
         assert_eq!(part_one(input), 5);
+    }
+
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_two(input), 7);
     }
 }

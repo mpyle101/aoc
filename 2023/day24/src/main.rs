9751decfd3b9e8d694afbfc0b3092b5c0db27f5b
Michael Pyle <mpyle101@gmail.com>
2024-01-10 20:28:29 -0700

    No more Z3, more understandable geometry version

--- 2023/day24/src/main.rs
+++ 2023/day24/src/main.rs
@@ -11,10 +11,6 @@ fn main()
     let t = Instant::now();
     let result = part_two(input);
     println!("Part 2 (mb): {} ({:?})", result, t.elapsed());
-
-    let t = Instant::now();
-    let result = part_two_z3(input);
-    println!("Part 2 (z3): {} ({:?})", result, t.elapsed());
 }
 
 fn part_one(input: &str) -> u32
@@ -46,9 +42,11 @@ fn part_two(input: &str) -> i64
     let mut vy = HashSet::new();
     let mut vz = HashSet::new();
 
+    let mut pair = [stones[0], stones[1]];
     for (a, i) in stones.iter().zip(1..) {
         for b in stones.iter().skip(i) {
             if vx.len() != 1 && a.v[0] == b.v[0] {
+                pair = [*a, *b];
                 let dv = (-1000..=1000)
                     .filter(|v| *v != a.v[0])
                     .filter(|v| (a.p[0] - b.p[0]) % (v - a.v[0]) == 0)
@@ -73,80 +71,98 @@ fn part_two(input: &str) -> i64
     }
 
     let rv = [
-        *vx.iter().next().unwrap() as f64,
-        *vy.iter().next().unwrap() as f64,
-        *vz.iter().next().unwrap() as f64
+        *vx.iter().next().unwrap(),
+        *vy.iter().next().unwrap(),
+        *vz.iter().next().unwrap(),
     ];
 
-    // Get slopes of lines formed by subtracting rock velocity
-    // from stone velocity.
-    let a = stones[0].real();
-    let b = stones[1].real();
-    let ma = (a.1[1] - rv[1]) / (a.1[0] - rv[0]);
-    let mb = (b.1[1] - rv[1]) / (b.1[0] - rv[0]);
+    // Get the difference in x position between a pair of
+    // stones with the same x velocity.
+    let dx = pair[0].p[0] - pair[1].p[0];
 
-    // y = mx + b => b = y - mx
-    let ba = a.0[1] - ma * a.0[0];
-    let bb = b.0[1] - mb * b.0[0];
+    // Calculate the # of jumps to cover that distance given
+    // our rocks velocity.
+    let ep = dx / (pair[0].v[0] - rv[0]);
 
-    // This part I don't quite get.
-    let x = (bb - ba) / (ma - mb);
-    let y = ma * x + ba;
-    let t = (x - a.0[0]) / (a.1[0] - rv[0]);
-    let z = a.0[2] + (a.1[2] - rv[2]) * t;
+    // Get the y distance traveled in that many jumps.
+    let dy = ep * rv[1];
 
-    x.round() as i64 +
-    y.round() as i64 +
-    z.round() as i64
-}
+    // The y position of stone 1 will be y1 + t * y1_v and to
+    // hit stone 2 it will be y2 + (t + ep) * y2_v. That distance
+    // needs to be dy which gives us:
+    // (y2 + (t + ep) * y2_v) - (y1 + t * y1_v) = dy
+    // We want to know t, the time it takes to get to the position.
+    let y1  = pair[0].p[1];
+    let y2  = pair[1].p[1];
+    let yv1 = pair[0].v[1];
+    let yv2 = pair[1].v[1];
+    let t = (y2 - y1 - dy + ep * yv2) / (yv1 - yv2);
 
-fn part_two_z3(input: &str) -> i64
-{
-    use z3::ast::{Ast, Int};
-    use z3::{Config, Context, SatResult, Solver};
+    // Calculate where the stone will be at time t, as our rock
+    // will be there at the same time.
+    let rp_t = [
+        pair[0].p[0] + t * pair[0].v[0],
+        pair[0].p[1] + t * pair[0].v[1],
+        pair[0].p[2] + t * pair[0].v[2],
+    ];
 
-    let stones = load(input);
-
-    let ctx = Context::new(&Config::new());
-    let solver = Solver::new(&ctx);
-
-    let px = Int::new_const(&ctx, "px");
-    let py = Int::new_const(&ctx, "py");
-    let pz = Int::new_const(&ctx, "pz");
-    let vx = Int::new_const(&ctx, "vx");
-    let vy = Int::new_const(&ctx, "vy");
-    let vz = Int::new_const(&ctx, "vz");
-    
-    let zero = Int::from_i64(&ctx, 0);
-
-    for (n, st) in stones.iter().enumerate() {
-        let t = Int::new_const(&ctx, format!("t{n}"));
-
-        let x1 = &px + (&vx * &t);
-        let y1 = &py + (&vy * &t);
-        let z1 = &pz + (&vz * &t);
-        let x2 = st.p[0] + (st.v[0] * &t);
-        let y2 = st.p[1] + (st.v[1] * &t);
-        let z2 = st.p[2] + (st.v[2] * &t);
-
-        solver.assert(&t.ge(&zero));
-        solver.assert(&x1._eq(&x2));
-        solver.assert(&y1._eq(&y2));
-        solver.assert(&z1._eq(&z2));
-    }
-
-    assert_eq!(solver.check(), SatResult::Sat);
-
-    let model = solver.get_model().unwrap();
-    let res_px = model.eval(&px, true).unwrap();
-    let res_py = model.eval(&py, true).unwrap();
-    let res_pz = model.eval(&pz, true).unwrap();
-    let x = res_px.as_i64().unwrap();
-    let y = res_py.as_i64().unwrap();
-    let z = res_pz.as_i64().unwrap();
+    // The starting position for our rock is then the position at
+    // t = 0 which is the position above minus the rock's distance
+    // traveled over time t.
+    let x = rp_t[0] - t * rv[0];
+    let y = rp_t[1] - t * rv[1];
+    let z = rp_t[2] - t * rv[2];
 
     x + y + z
 }
+
+// fn part_two_z3(input: &str) -> i64
+// {
+//     use z3::ast::{Ast, Int};
+//     use z3::{Config, Context, SatResult, Solver};
+
+//     let stones = load(input);
+
+//     let ctx = Context::new(&Config::new());
+//     let solver = Solver::new(&ctx);
+
+//     let px = Int::new_const(&ctx, "px");
+//     let py = Int::new_const(&ctx, "py");
+//     let pz = Int::new_const(&ctx, "pz");
+//     let vx = Int::new_const(&ctx, "vx");
+//     let vy = Int::new_const(&ctx, "vy");
+//     let vz = Int::new_const(&ctx, "vz");
+    
+//     let zero = Int::from_i64(&ctx, 0);
+
+//     for (n, st) in stones.iter().enumerate() {
+//         let t = Int::new_const(&ctx, format!("t{n}"));
+
+//         let x1 = &px + (&vx * &t);
+//         let y1 = &py + (&vy * &t);
+//         let z1 = &pz + (&vz * &t);
+//         let x2 = st.p[0] + (st.v[0] * &t);
+//         let y2 = st.p[1] + (st.v[1] * &t);
+//         let z2 = st.p[2] + (st.v[2] * &t);
+
+//         solver.assert(&t.ge(&zero));
+//         solver.assert(&x1._eq(&x2));
+//         solver.assert(&y1._eq(&y2));
+//         solver.assert(&z1._eq(&z2));
+//     }
+
+//     assert_eq!(solver.check(), SatResult::Sat);
+
+//     let model = solver.get_model().unwrap();
+//     let res_px = model.eval(&px, true).unwrap();
+//     let res_py = model.eval(&py, true).unwrap();
+//     let res_pz = model.eval(&pz, true).unwrap();
+//     let x = res_px.as_i64().unwrap();
+//     let y = res_py.as_i64().unwrap();
+//     let z = res_pz.as_i64().unwrap();
+
+//     x + y + z
+// }
 
 fn load(input: &str) -> Vec<Stone>
 {
@@ -173,15 +189,6 @@ fn load(input: &str) -> Vec<Stone>
 struct Stone {
     p: [i64;3],
     v: [i64;3],
-}
-impl Stone {
-    fn real(&self) -> ([f64;3], [f64;3])
-    {
-        (
-            [self.p[0] as f64, self.p[1] as f64, self.p[2] as f64],
-            [self.v[0] as f64, self.v[1] as f64, self.v[2] as f64],
-        )
-    }
 }
 
 fn crossings(stones: &[Stone], min: f32, max: f32) -> u32
@@ -237,7 +244,7 @@ mod tests {
     fn input_part_two()
     {
         let input = include_str!("../input.txt");
-        assert_eq!(part_two_z3(input), 669042940632377);
+        assert_eq!(part_two(input), 669042940632377);
     }
 
     #[test]
@@ -248,10 +255,10 @@ mod tests {
         assert_eq!(crossings(&stones, 7.0, 27.0), 2);
     }
 
-    #[test]
-    fn example_part_two()
-    {
-        let input = include_str!("../example.txt");
-        assert_eq!(part_two_z3(input), 47);
-    }
+    // #[test]
+    // fn example_part_two()
+    // {
+    //     let input = include_str!("../example.txt");
+    //     assert_eq!(part_two(input), 47);
+    // }
 }

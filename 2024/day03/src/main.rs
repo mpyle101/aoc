90c4967b979964d3f748d0f1fab23acf8d2c5b8e
Michael Pyle <mpyle101@gmail.com>
2024-12-03 17:16:46 -0700

    Use files for example data

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -117,14 +117,14 @@ mod tests {
     #[test]
     fn example_part_one()
     {
-        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let input = include_str!("../example1.txt");
         assert_eq!(part_one(input), 161);
     }
 
     #[test]
     fn example_part_two()
     {
-        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        let input = include_str!("../example2.txt");
         assert_eq!(part_two(input), 48);
     }
 }

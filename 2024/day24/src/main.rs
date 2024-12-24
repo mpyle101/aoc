ea25f6625707e4cb49e80714cde398fe844a24db
Michael Pyle <mpyle101@gmail.com>
2024-12-24 01:40:46 -0700

    A little refactor and start on part 2

--- 2024/day24/src/main.rs
+++ 2024/day24/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::manual_strip)]
+
 use std::collections::HashMap;
 
 type Wires<'a> = HashMap<&'a str, u64>;
@@ -12,37 +14,43 @@ fn main()
     let t = Instant::now();
     let result = part_one(input);
     println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-#[allow(clippy::manual_strip)]
 fn part_one(input: &str) -> u64
 {
-    let (s1, s2) = input.split_once("\n\n").unwrap();
-    let mut wires = s1.lines()
-        .filter_map(|line| line.split_once(": "))
-        .filter_map(|(w, v)| v.parse::<u64>().ok().map(|n| (w, n)))
-        .collect::<Wires>();
+    let (mut wires, gates) = load(input);
+    let z_wires = gates.keys()
+        .filter(|k| k.starts_with('z'))
+        .collect::<Vec<_>>();
 
-    let mut z_wires = Vec::new();
-    let gates = s2.lines()
-        .fold(Gates::new(), |mut m, line| {
-            let mut it = line.split(' ');
-            let a  = it.next().unwrap();
-            let op = match it.next().unwrap() {
-                "AND" => '&',
-                "OR"  => '|',
-                "XOR" => '^',
-                _ => unreachable!()
-            };
-    
-            let b  = it.next().unwrap();
-            it.next();
-            let c  = it.next().unwrap();
+    let mut z = 0;
+    for w in z_wires {
+        let n = evaluate(w, &mut wires, &gates);
+        let i = w[1..].parse::<u64>().unwrap();
+        if n == 1 { z |= 1 << i } else { z &= !(1 << i) }
+    }
 
-            if c.starts_with('z') { z_wires.push(c) }
-            m.insert(c, (a, b, op));
-            m
-        });
+    z
+}
+
+fn part_two(input: &str) -> u64
+{
+    let (mut wires, gates) = load(input);
+    let _x = wires.iter()
+        .filter(|(w, &n)| n == 1 && w.starts_with('x'))
+        .map(|(w, _)| w[1..].parse::<u64>().unwrap())
+        .fold(0, |x, i| x | 1 << i);
+    let _y = wires.iter()
+        .filter(|(w, &n)| n == 1 && w.starts_with('y'))
+        .map(|(w, _)| w[1..].parse::<u64>().unwrap())
+        .fold(0, |x, i| x | 1 << i);
+    let z_wires = gates.keys()
+        .filter(|k| k.starts_with('z'))
+        .collect::<Vec<_>>();
 
     let mut z = 0;
     for w in z_wires {
@@ -73,6 +81,36 @@ fn evaluate<'a>(w: &'a str, wires: &mut Wires<'a>, gates: &Gates<'a>) -> u64
     } else {
         unreachable!()
     }
+}
+
+fn load(input: &str) -> (Wires, Gates)
+{
+    let (s1, s2) = input.split_once("\n\n").unwrap();
+    let wires = s1.lines()
+        .filter_map(|line| line.split_once(": "))
+        .filter_map(|(w, v)| v.parse::<u64>().ok().map(|n| (w, n)))
+        .collect::<Wires>();
+
+    let gates = s2.lines()
+        .fold(Gates::new(), |mut m, line| {
+            let mut it = line.split(' ');
+            let a  = it.next().unwrap();
+            let op = match it.next().unwrap() {
+                "AND" => '&',
+                "OR"  => '|',
+                "XOR" => '^',
+                _ => unreachable!()
+            };
+            let b  = it.next().unwrap();
+            it.next();
+            let c  = it.next().unwrap();
+
+            m.insert(c, (a, b, op));
+            m
+        });
+
+    (wires, gates)
+ 
 }
 
 

bdc559c3653d993a4e2b237e617c591f9c77882d
Michael Pyle <mpyle101@gmail.com>
2024-12-26 12:29:24 -0700

    2020 Day05 Part 2

--- 2020/day05/src/main.rs
+++ 2020/day05/src/main.rs
@@ -20,17 +20,36 @@ fn part_one(input: &str) -> u32
 
 fn part_two(input: &str) -> u32
 {
+    let mut seats = input.lines()
+        .map(seat)
+        .collect::<Vec<_>>();
+    seats.sort_unstable();
 
-    0
+    // take_while runs until the test fails so the last element
+    // will be the last value for which the predicate returned
+    // true. We know it's a full flight so the last true test
+    // must be the seat before the open seat.
+    // Ex: 10 11 12 14
+    // 11 => 11 - 10 == 1  (true)
+    // 12 => 12 - 11 == 1  (true)
+    // 14 => 14 - 12 == 2  (false)
+    // The last true is for 12 - 11, so the iterations last
+    // value is 12 and the missing one is 13
+    seats.iter()
+        .enumerate()
+        .skip(1)
+        .take_while(|(i, n)| **n - seats[i-1] == 1)
+        .last()
+        .map_or(0, |(_, n)| n + 1)
 }
 
 fn seat(bp: &str) -> u32
 {
-    bp.chars().rev()
+    bp.chars()
+        .rev()
         .zip(0..)
-        .fold(0, |n, (c, i)| {
-            n | if c == 'F' || c == 'L' { 1 << i } else { n }
-        })
+        .filter(|&(c, _)| c == 'B' || c == 'R')
+        .fold(0, |n, (_, i)| n | 1 << i)
 }
 
 
@@ -41,14 +60,14 @@ mod tests {
     #[test]
     fn input_part_one()
     {
-        let input = load(include_str!("../input.txt"));
-        assert_eq!(part_one(&input), 998);
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 998);
     }
 
     #[test]
     fn input_part_two()
     {
-        let input = load(include_str!("../input.txt"));
-        assert_eq!(part_two(&input), 676);
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 676);
     }
 }

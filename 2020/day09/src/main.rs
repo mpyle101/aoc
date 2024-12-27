f6f79b88bef15ceda348f4abb9fae363487b45ee
Michael Pyle <mpyle101@gmail.com>
2024-12-27 08:07:42 -0700

    2020 Day09 Redux

--- 2020/day09/src/main.rs
+++ 2020/day09/src/main.rs
@@ -1,45 +1,67 @@
-fn main() {
-    let xmas = load(include_str!("./xmas.txt"));
+fn main()
+{
+    use std::time::Instant;
 
-    let num = part_one(&xmas);
-    println!("Part 1: {num}");
+    let input = include_str!("../input.txt");
 
-    let num = part_two(&xmas, num);
-    println!("Part 2: {num}");
+    let t = Instant::now();
+    let result = part_one(input, 25, 25);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input, 25, 25);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn load(input: &str) -> Vec<u64> {
-    input.lines().map(|l| l.parse::<u64>().unwrap()).collect()
+fn part_one(input: &str, preamble: usize, lookback: usize) -> u64
+{
+    let xmas = input.lines()
+        .filter_map(|s| s.parse::<u64>().ok())
+        .collect::<Vec<_>>();
+
+    step_one(&xmas, preamble, lookback)
 }
 
-fn part_one(xmas: &[u64]) -> u64 {
-    use itertools::Itertools;
+fn part_two(input: &str, preamble: usize, lookback: usize) -> u64
+{
+    let xmas = input.lines()
+        .filter_map(|s| s.parse::<u64>().ok())
+        .collect::<Vec<_>>();
 
-    let mut iter = xmas.iter().enumerate().skip(25)
-        .skip_while(|(i, n)| xmas[i - 25..*i].iter()
-            .combinations(2).any(|v| v[0] + v[1] == **n));
-
-    *(iter.next().unwrap().1)
-}
-
-fn part_two(xmas: &[u64], n: u64) -> u64 {
-    let mut s = 0;
-    let mut e = 0;
-    let mut sum = xmas[e];
-    while sum != n {
-        while sum < n {
-            e += 1;
-            sum += xmas[e];
-        }
-        while sum > n {
-            sum -= xmas[s];
-            s += 1;
+    let n = step_one(&xmas, preamble, lookback);
+    for i in 0..xmas.len()-1 {
+        let mut j = i + 1;
+        let mut sum = xmas[i];
+        while sum < n { sum += xmas[j]; j += 1 }
+        if sum == n {
+            let mut seq = xmas[i..j-1].to_vec();
+            seq.sort();
+            return seq.first().unwrap() + seq.last().unwrap();
         }
     }
 
-    let min = xmas[s..e].iter().min().unwrap();
-    let max = xmas[s..e].iter().max().unwrap();
-    min + max
+    0
+}
+
+fn step_one(xmas: &[u64], preamble: usize, lookback: usize) -> u64
+{
+    (preamble..xmas.len())
+        .take_while(|&i| valid(xmas[i], &xmas[i-lookback..i]))
+        .last()
+        .map_or(0, |i| xmas[i+1])
+}
+
+fn valid(n: u64, xmas: &[u64]) -> bool
+{
+    for i in 0..xmas.len()-1 {
+        for j in i..xmas.len() {
+            if n == xmas[i] + xmas[j] {
+                return true
+            }
+        }
+    }
+
+    false
 }
 
 
@@ -48,21 +70,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let xmas = load(include_str!("./xmas.txt"));
-
-        let num = part_one(&xmas);
-        assert_eq!(num, 1124361034);
-
-        let num = part_two(&xmas, num);
-        assert_eq!(num, 129444555);
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input, 25, 25), 1124361034);
     }
 
     #[test]
-    fn small_weakness() {
-        let xmas = [35,20,15,25,47,40,62,55,65,95];
-
-        let weakness = part_two(&xmas, 127);
-        assert_eq!(weakness, 62);
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input, 25, 25), 129444555);
     }
+
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one(input, 5, 5), 127);
+    }
+
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_two(input, 5, 5), 62);
+    }
+
 }

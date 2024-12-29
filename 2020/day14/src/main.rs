f9c2a61c7ea8f2b441c8f856612d14be645ad308
Michael Pyle <mpyle101@gmail.com>
2024-12-28 23:11:16 -0700

    2020 Day14 Redux

--- 2020/day14/src/main.rs
+++ 2020/day14/src/main.rs
@@ -1,169 +1,126 @@
-fn main() {
-    let cmds = load(include_str!("./program.txt"));
+fn main()
+{
+    use std::time::Instant;
 
-    println!("Part 1: {}", part_one(&cmds));
-    println!("Part 2: {}", part_two(&cmds));
+    let input = include_str!("../input.txt");
+
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(cmds: &[Cmd]) -> u64 {
+fn part_one(input: &str) -> u64
+{
     use std::collections::HashMap;
-
-    let mut memory = HashMap::new();
-
-    let mut mask = (0u64, 0u64);
-    for cmd in cmds {
-        match cmd {
-            Cmd::Mem { addr, val } => {
-                memory.insert(addr, (val | mask.1) & mask.0);
-            },
-            Cmd::Mask { pattern } => {
-                mask = generate_mask(pattern);
-            }
-        }
-    }
-
-    memory.values().sum()
-}
-
-fn part_two(cmds: &[Cmd]) -> u64 {
-    use std::collections::HashMap;
-
-    let mut memory = HashMap::new();
 
     let mut mask = "";
-    for cmd in cmds {
-        match cmd {
-            Cmd::Mem { addr, val } => {
-                let addrs = apply_mask(mask, addr);
-                addrs.iter().for_each(|a| { memory.insert(*a, *val); })
-            },
-            Cmd::Mask { pattern } => mask = pattern,
-        }
-    }
+    let mut memory = HashMap::new();
+    input.lines()
+        .flat_map(|line| line.split_once(" = "))
+        .for_each(|(s1, s2)| {
+            if s1 == "mask" {
+                mask = s2
+            } else {
+                let n = s2.parse::<u64>().unwrap();
+                let m = s1[4..s1.len()-1].parse::<u32>().unwrap();
+                memory.insert(m, apply(mask, n));
+            }
+        });
 
     memory.values().sum()
 }
 
-enum Cmd<'a> {
-    Mem { addr: u64, val: u64 },
-    Mask { pattern: &'a str },
-}
-
-fn load(input: &str) -> Vec<Cmd> {
-    input.lines()
-        .map(|s| s.split(" = ").collect::<Vec<&str>>())
-        .map(|v| if v[0] == "mask" { 
-            Cmd::Mask { pattern: v[1] }
-        } else {
-            Cmd::Mem {
-                addr: v[0][4..v[0].len() - 1].parse::<u64>().unwrap(),
-                val: v[1].parse::<u64>().unwrap()
-            }
-        })
-        .collect()
-}
-
-fn generate_mask(pattern: &str) -> (u64, u64) {
-    let (mut zeros, ones) = pattern.as_bytes().iter().rev()
-        .enumerate()
-        .fold((0, 0), |(zeros, ones), (i, b)|
-            match b {
-                b'1' => (zeros, ones | 1 << i),
-                b'0' => (zeros | 1 << i, ones),
-                _    => (zeros, ones)
-            }
-        );
-
-    // Flip the bits and clear those beyond 36
-    zeros = !zeros;
-    (36..64).for_each(|n| zeros &= !(1 << n));
-
-    (zeros, ones)
-}
-
-fn apply_mask(pattern: &str, addr: &u64) -> Vec<u64> {
+fn part_two(input: &str) -> u64
+{
     use std::collections::HashMap;
-    let mut cache = HashMap::new();
 
-    let addr_bits = format!("{addr:036b}");
-    let result = pattern.as_bytes().iter().zip(
-        addr_bits.as_bytes().iter())
-        .map(|(p, a)| match p {
-            b'0' => *a,
-            b'1' => b'1',
-            _    => b'X'
-        })
-        .collect::<Vec<_>>();
-        
-    let pos: Vec<_> = result.iter().enumerate()
-        .filter(|(_, &v)| v == b'X').map(|(i, _)| i)
-        .collect();
-    let cmb = cache.entry(pos.len()).or_insert_with_key(|&k| combinations(k));
-
-    cmb.iter().map(|v| {
-        let mut m = result.clone();
-        v.iter().zip(pos.iter()).for_each(|(&b, &i)| m[i] = b);
-        m
-    })
-    .map(|v| generate_value(&v))
-    .collect()
-}
-
-fn combinations(len: usize) -> Vec<Vec<u8>> {
-    let v = Vec::new();
-    let mut c = Vec::new();
-    generate_combination(len, v, &mut c);
-
-    c
-}
-
-fn generate_value(bits: &[u8]) -> u64 {
-    bits.iter().rev()
-        .enumerate()
-        .fold(0, |acc, (i, b)|
-            match b {
-                b'1' => acc | 1 << i,
-                _    => acc
+    let mut mask = vec![];
+    let mut memory = HashMap::new();
+    input.lines()
+        .flat_map(|line| line.split_once(" = "))
+        .for_each(|(s1, s2)| {
+            if s1 == "mask" {
+                mask = s2.chars().collect::<Vec<_>>();
+            } else {
+                let n = s2.parse::<u64>().unwrap();
+                let m = s1[4..s1.len()-1].parse::<u64>().unwrap();
+                decode(&mask, m)
+                    .iter()
+                    .for_each(|&a| { memory.insert(a, n); });
             }
-        )
+        });
+
+    memory.values().sum()
 }
 
-fn generate_combination(len: usize, v: Vec<u8>, c: &mut Vec<Vec<u8>>) {
-    if len == 0 {
-        c.push(v);
+fn apply(mask: &str, n: u64) -> u64
+{
+    mask.chars()
+        .rev()
+        .enumerate()
+        .filter(|(_, c)| *c != 'X')
+        .fold(n, |acc, (i, c)| {
+            if c == '1' {
+                acc | (1 << i)
+            } else {
+                acc & !(1 << i)
+            }
+        })
+}
+
+fn decode(mask: &[char], m: u64) -> Vec<u64>
+{
+    if mask.is_empty() {
+        vec![m]
     } else {
-        let mut w = v.clone(); w.push(b'0');
-        generate_combination(len - 1, w, c);
-        let mut w = v; w.push(b'1');
-        generate_combination(len - 1, w, c);
+        let i = mask.len() - 1;
+        if mask[0] == '0' {
+            decode(&mask[1..], m)
+        } else if mask[0] == '1' {
+            decode(&mask[1..], m | (1 << i))
+        } else {
+            let mut v = vec![];
+            v.extend(decode(&mask[1..], m | (1 << i)));
+            v.extend(decode(&mask[1..], m & !(1 << i)));
+            v
+        }
     }
 }
 
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let cmds = load(include_str!("./program.txt"));
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 12512013221615);
+    }
 
-    let checksum = part_one(&cmds);
-    assert_eq!(checksum, 12512013221615);
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 3905642473893);
+    }
 
-    let checksum = part_two(&cmds);
-    assert_eq!(checksum, 3905642473893);
-  }
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example1.txt");
+        assert_eq!(part_one(input), 165);
+    }
 
-  #[test]
-  fn example_1() {
-    let cmds = load("\
-mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0");
-
-    let checksum = part_one(&cmds);
-    assert_eq!(checksum, 165);
-  }
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example2.txt");
+        assert_eq!(part_two(input), 208);
+    }
 }

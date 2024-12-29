a9c907e5b4bae5e441a9747f2623eb6d0ee9ebb4
Michael Pyle <mpyle101@gmail.com>
2024-12-28 18:45:55 -0700

    2020 Day12 Redux

--- 2020/day12/src/main.rs
+++ 2020/day12/src/main.rs
@@ -1,121 +1,149 @@
-use std::str;
+fn main()
+{
+    use std::time::Instant;
 
-fn main() {
-    let moves = include_str!("./moves.txt");
+    let input = include_str!("../input.txt");
 
-    println!("Part1: {}", part_one(moves));
-    println!("Part2: {}", part_two(moves));
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(moves: &str) -> i32 {
-    use Direction::*;
-
-    let mut facing = Facing::East;
-
-    let dest = moves.lines()
-        .fold((0i32, 0i32), |acc, line| {
-            let cmd = line.as_bytes()[0];
-            let val = (line[1..]).parse::<i32>().unwrap();
-
-            match cmd {
-                b'N' => (acc.0, acc.1 - val),
-                b'S' => (acc.0, acc.1 + val),
-                b'E' => (acc.0 + val, acc.1),
-                b'W' => (acc.0 - val, acc.1),
-                b'F' => advance(&facing, acc, val),
-                b'L' => { facing = rotate(&facing, Left, val); acc },
-                b'R' => { facing = rotate(&facing, Right, val); acc },
-                _    => panic!("Invalid command")
+fn part_one(input: &str) -> u32
+{
+    let (_, p) = input.lines()
+        .map(|line| {
+            let c = line.chars().next().unwrap();
+            let n = line[1..].parse::<i32>().unwrap();
+            (c, n)
+        })
+        .fold(('E', (0, 0)), |(d, p), (c, n)| {
+            match c {
+                'F'             => (d, advance(d, n, p)),
+                'R'|'L'         => (rotate(d, c, n), p),
+                'N'|'S'|'E'|'W' => (d, advance(c, n, p)),
+                _ => unreachable!()
             }
         });
-    
-    dest.0.abs() + dest.1.abs()
+
+    md((0, 0), p)
 }
 
-fn part_two(moves: &str) -> i32 {
-    let (ship, _) = moves.lines()
-        .fold(((0, 0), (10, -1)), |(ship, wp), line| {
-            let cmd = line.as_bytes()[0];
-            let val = (line[1..]).parse::<i32>().unwrap();
-
-            match cmd {
-                b'N' => (ship, (wp.0, wp.1 - val)),
-                b'S' => (ship, (wp.0, wp.1 + val)),
-                b'E' => (ship, (wp.0 + val, wp.1)),
-                b'W' => (ship, (wp.0 - val, wp.1)),
-                b'R' => (ship, rotate_waypoint(wp, val)),
-                b'L' => (ship, rotate_waypoint(wp, 360 - val)),
-                b'F' => ((ship.0 + wp.0 * val, ship.1 + wp.1 * val), wp),
-                _    => panic!("Invalid command")
+fn part_two(input: &str) -> u32
+{
+    let (p, _) = input.lines()
+        .map(|line| {
+            let c = line.chars().next().unwrap();
+            let n = line[1..].parse::<i32>().unwrap();
+            (c, n)
+        })
+        .fold(((0, 0), (10, 1)), |(sp, wp), (c, n)| {
+            match c {
+                'F'             => move_ship(n, sp, wp),
+                'R'|'L'         => (sp, rotate_wp(c, n, sp, wp)),
+                'N'|'S'|'E'|'W' => (sp, advance(c, n, wp)),
+                _ => unreachable!()
             }
         });
-    
-    ship.0.abs() + ship.1.abs()
+
+    md((0, 0), p)
 }
 
-fn advance(facing: &Facing, pos: (i32, i32), dist: i32) -> (i32, i32) {
-    match facing {
-        Facing::North => (pos.0, pos.1 - dist),
-        Facing::South => (pos.0, pos.1 + dist),
-        Facing::East  => (pos.0 + dist, pos.1),
-        Facing::West  => (pos.0 - dist, pos.1),
-    }
+fn move_ship(
+    n: i32,
+    (sx, sy): (i32, i32),   // sp
+    (wx, wy): (i32, i32)    // wp
+) -> ((i32, i32), (i32, i32))
+{
+    let dx = wx - sx;
+    let dy = wy - sy;
+    let sp = (sx + dx * n, sy + dy * n);
+    let wp = (sp.0 + dx, sp.1 + dy);
+
+    (sp, wp)
 }
 
-fn rotate(facing: &Facing, dir: Direction, deg: i32) -> Facing {
-    use Direction::*;
-
-    let d = match dir {
-        Right => (*facing as u8).rotate_right((deg as u32 / 90) * 2),
-        Left  => (*facing as u8).rotate_left((deg as u32/ 90) * 2),
+fn rotate_wp(d: char, n: i32, (sx, sy): (i32, i32), (wx, wy): (i32, i32)) -> (i32, i32)
+{
+    let n = n / 90;
+    let p = (wx - sx, wy - sy);
+    let (x, y) = if d == 'R' {
+        (0..n).fold(p, |(x, y), _| (y, -x))
+    } else {
+        (0..n).fold(p, |(x, y), _| (-y, x))
     };
 
+    (x + sx, y + sy)
+}
+
+fn md((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> u32
+{
+    x1.abs_diff(x2) + y1.abs_diff(y2)
+}
+
+static DIRS: [char;4] = ['E', 'S', 'W', 'N'];
+
+fn rotate(d: char, c: char, n: i32) -> char
+{
+    let n = n / 90;
+    let i = match d {
+        'E' => 0,
+        'S' => 1,
+        'W' => 2,
+        'N' => 3,
+         _  => unreachable!()
+    };
+    let a = if c == 'R' { i + n } else { i - n };
+    let i = a.rem_euclid(4) as usize;
+
+    DIRS[i]
+}
+
+fn advance(d: char, n: i32, (x, y): (i32, i32)) -> (i32, i32)
+{
     match d {
-        0b10000000 => Facing::North,
-        0b00100000 => Facing::East,
-        0b00001000 => Facing::South,
-        0b00000010 => Facing::West,
-        _ => panic!("Unknown rotate result: {d:#010b}")
+        'N' => (x, y + n),
+        'S' => (x, y - n),
+        'E' => (x + n, y),
+        'W' => (x - n, y),
+         _  => unreachable!()
     }
-}
-
-fn rotate_waypoint(wp: (i32, i32), deg: i32) -> (i32, i32) {
-    let steps = deg / 90;
-    (0..steps).fold(wp, |acc, _| (-acc.1, acc.0))
-}
-
-// E10  N 4  10   -4
-// E 4  S10   4   10
-// W10  S 4 -10    4
-// W 4  N10  -4  -10
-
-#[derive(Debug)]
-enum Direction {
-    Right,
-    Left,
-}
-
-#[derive(Clone, Copy)]
-enum Facing {
-    North = 0b10000000,
-    East  = 0b00100000,
-    South = 0b00001000,
-    West  = 0b00000010,
 }
 
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let moves = include_str!("./moves.txt");
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 562);
+    }
 
-    let dist = part_one(moves);
-    assert_eq!(dist, 562);
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 101860);
+    }
 
-    let dist = part_two(moves);
-    assert_eq!(dist, 101860);
-  }
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one(input), 25);
+    }
+
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_two(input), 286);
+    }
 }

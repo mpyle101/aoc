84dcc7809b2a8d1af8b39356f6e64c148679231f
Michael Pyle <mpyle101@gmail.com>
2024-12-30 14:53:01 -0700

    2020 Day15 Redux

--- 2020/day15/src/main.rs
+++ 2020/day15/src/main.rs
@@ -1,82 +1,85 @@
-fn main() {
-    let numbers = [1, 0, 15, 2, 10, 13];
+fn main()
+{
+    use std::time::Instant;
 
-    println!("Part 1: {}", part_one(&numbers));
-    println!("Part 2: {}", part_two(&numbers));
+    let input = include_str!("../input.txt");
+
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(nums: &[usize]) -> usize {
-    memory_game(nums, 2020)
+fn part_one(input: &str) -> u32
+{
+    let v = input.split(',')
+      .flat_map(|s| s.parse::<u32>())
+      .collect::<Vec<_>>();
+
+    play(&v, 2020)
 }
 
-fn part_two(nums: &[usize]) -> usize {
-    memory_game(nums, 30000000)
+fn part_two(input: &str) -> u32
+{
+    let v = input.split(',')
+        .flat_map(|s| s.parse::<u32>())
+        .collect::<Vec<_>>();
+
+    play(&v, 30000000)
 }
 
-fn memory_game(nums: &[usize], iteration: usize) -> usize {
+fn play(nums: &[u32], iterations: u32) -> u32
+{
     use std::collections::HashMap;
 
-    let mut spoken: HashMap<_,_> = nums.iter().enumerate()
-        .map(|(i, n)| (*n, (i+1, i+1)))
+    let mut spoken: HashMap<_,_> = nums.iter()
+        .zip(0..)
+        .map(|(n, i)| (*n, (i+1, i+1)))
         .collect();
 
-    let mut last  = nums[nums.len() - 1];
-    let mut count = spoken.len() + 1;
-    while count <= iteration {
-        let t = spoken.entry(last).or_insert((count, count));
-        last  = t.1 - t.0;
-        let n = spoken.entry(last).or_insert((count, count));
-        *n = (n.1, count);
-        count += 1;
-    };
-
-    last
+    let last  = nums[nums.len() - 1];
+    let turn = (spoken.len() + 1) as u32;
+    (turn..=iterations)
+        .fold(last, |last, i| {
+            let (a, b) = spoken.entry(last).or_insert((i, i));
+            let n = *b - *a;
+            let e = spoken.entry(n).or_insert((i, i));
+            *e = (e.1, i);
+            n
+        })
 }
 
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let numbers = [1, 0, 15, 2, 10, 13];
-    
-    let n = part_one(&numbers);
-    assert_eq!(n, 211);
-    
-    let n = part_two(&numbers);
-    assert_eq!(n, 2159626);
-  }
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 211);
+    }
 
-  #[test]
-  fn examples_2020() {
-    let numbers = [0,3,6];
-    let n = part_one(&numbers);
-    assert_eq!(n, 436);
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 2159626);
+    }
 
-    let numbers = [1,3,2];
-    let n = part_one(&numbers);
-    assert_eq!(n, 1);
+    #[test]
+    fn example_part_one()
+    {
+        assert_eq!(part_one("3,1,2"), 1836);
+    }
 
-    let numbers = [2,1,3];
-    let n = part_one(&numbers);
-    assert_eq!(n, 10);
-
-    let numbers = [1,2,3];
-    let n = part_one(&numbers);
-    assert_eq!(n, 27);
-
-    let numbers = [2,3,1];
-    let n = part_one(&numbers);
-    assert_eq!(n, 78);
-
-    let numbers = [3,2,1];
-    let n = part_one(&numbers);
-    assert_eq!(n, 438);
-
-    let numbers = [3,1,2];
-    let n = part_one(&numbers);
-    assert_eq!(n, 1836);
-  }
+    #[test]
+    fn example_part_two()
+    {
+        assert_eq!(part_two("2,3,1"), 6895259);
+    }
 }

e7a6d5292ade7672933db4538263866adcfa1e86
Michael Pyle <mpyle101@gmail.com>
2024-12-30 22:34:48 -0700

    2020 Day24 Redux

--- 2020/day24/src/main.rs
+++ 2020/day24/src/main.rs
@@ -1,14 +1,62 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-fn main() {
-    let input = include_str!("./input.txt");
+fn main()
+{
+    use std::time::Instant;
 
-    let black = part_one(input);
-    println!("Part 1: {}", black.len());
-    println!("Part 2: {}", part_two(&black));
+    let input = include_str!("../input.txt");
+
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(input: &str) -> HashSet<(i32, i32)> {
+fn part_one(input: &str) -> usize
+{
+    let tiles = load(input);
+
+    tiles.len()
+}
+
+fn part_two(input: &str) -> usize
+{
+    let tiles = load(input);
+    (0..100).fold(tiles, |tiles, _| {
+        let mut white = HashSet::new();
+        let mut black = HashSet::new();
+
+        tiles.iter().for_each(|tile| {
+            let adjacent = NEIGHBORS.iter()
+                .map(|(dx, dy)| (tile.0 + dx, tile.1 + dy))
+                .fold(0, |acc, p|
+                    if tiles.contains(&p) { 
+                        acc + 1
+                    } else {
+                        white.insert(p);
+                        acc
+                    }
+                );
+            if adjacent == 1 || adjacent == 2 {
+                black.insert(*tile);
+            }
+        });
+        white.iter()
+            .filter(|tile| adjacent(&tiles, tile) == 2)
+            .for_each(|tile| { black.insert(*tile); });
+
+        black
+    })
+    .len()
+}
+
+fn load(input: &str) -> HashSet<(i32, i32)>
+{
+    use std::collections::HashMap;
+
     input.lines()
         .fold(HashMap::new(), |mut acc, s| {
             let (tile, _) = s.chars().fold(((0, 0), '_'), |((x, y), p), c|
@@ -20,7 +68,7 @@ fn part_one(input: &str) -> HashSet<(i32, i32)> {
                     ('s', 'w') => (x - 1, y + 1),
                     ( _ , 'e') => (x + 2, y),
                     ( _ , 'w') => (x - 2, y),
-                             _ => (x, y)
+                            _ => (x, y)
                 }, c)
             );
             *acc.entry(tile).or_insert(0) += 1;
@@ -32,31 +80,6 @@ fn part_one(input: &str) -> HashSet<(i32, i32)> {
         .collect()
 }
 
-fn part_two(tiles: &HashSet<(i32, i32)>) -> usize {
-    let black = (0..100).fold(tiles.clone(), |blk, _| {
-        let mut white = HashSet::new();
-        let mut black = HashSet::new();
-
-        blk.iter().for_each(|tile| {
-            let adjacent = NEIGHBORS.iter()
-                .map(|(dx, dy)| (tile.0 + dx, tile.1 + dy))
-                .fold(0, |acc, p|
-                    if blk.contains(&p) { acc + 1 } else { white.insert(p); acc }
-                );
-            if adjacent == 1 || adjacent == 2 {
-                black.insert(*tile);
-            }
-        });
-        white.iter()
-            .filter(|tile| adjacent(&blk, tile) == 2)
-            .for_each(|tile| { black.insert(*tile); });
-
-        black
-    });
-
-    black.len()
-}
-
 const NEIGHBORS: [(i32, i32); 6] = [
     (2, 0), (-2, 0), (1, -1), (-1, -1), (1, 1), (-1, 1)
 ];
@@ -66,30 +89,35 @@ fn adjacent(black: &HashSet<(i32, i32)>, (x, y): &(i32, i32)) -> usize {
 }
 
 
-
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let input = include_str!("./input.txt");
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 254);
+    }
 
-    let black = part_one(input);
-    assert_eq!(black.len(), 254);
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 3697);
+    }
 
-    let tiles = part_two(&black);
-    assert_eq!(tiles, 3697);
-  }
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one(input), 10);
+    }
 
-  #[test]
-  fn small() {
-    let input = include_str!("./test.txt");
-
-    let black = part_one(input);
-    assert_eq!(black.len(), 10);
-
-    let tiles = part_two(&black);
-    assert_eq!(tiles, 2208);
-  }
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_two(input), 2208);
+    }
 }

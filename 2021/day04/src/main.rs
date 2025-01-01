8deadd5a8f142f5e75c314804faa9a54a3f0450f
Michael Pyle <mpyle101@gmail.com>
2024-12-31 20:23:51 -0700

    2021 Day04 Redux

--- 2021/day04/src/main.rs
+++ 2021/day04/src/main.rs
@@ -1,97 +1,163 @@
-fn main() {
-    use std::fs;
+fn main()
+{
     use std::time::Instant;
 
-    let (cards, mut boards) = load(&fs::read_to_string("./input.txt").unwrap());
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let (winner, squid) = doit(&cards, &mut boards);
-    let t2 = Instant::now();
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
 
-    println!("Part 1: {winner}");
-    println!("Part 2: {squid}");
-    println!("{:?}", t2 - t1);
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn load(input: &str) -> (Vec<i32>, Vec<Vec<(i32, bool)>>) {
-    let mut iter = input.split("\n\n");
-    let cards = iter
-        .next()
-        .unwrap()
-        .split(',')
-        .map(|s| s.parse::<i32>().unwrap())
-        .collect::<Vec<_>>();
+fn part_one(input: &str) -> u32
+{
+    let (nums, boards) = load(input);
+    let mut marked = vec![0;boards.len()];
 
-    let boards = iter.fold(Vec::new(), |mut v, s| {
-        let m = s
-            .split_whitespace()
-            .map(|s| (s.parse::<i32>().unwrap(), false))
-            .collect::<Vec<_>>();
-        v.push(m);
-        v
-    });
+    let mut it = nums.iter();
+    let (n, i) = loop {
+        if let Some(&n) = it.next() {
+            mark(n, &boards, &mut marked);
+            if let Some(i) = bingo(&marked) {
+                break (n, i)
+            }
+        } else {
+            break (0, 0)
+        }
+    };
 
-    (cards, boards)
-}
-
-fn doit(cards: &[i32], boards: &mut Vec<Vec<(i32, bool)>>) -> (i32, i32) {
-    // Draw the first 5 cards
-    let mut iter = cards.iter();
-    (0..4).for_each(|_| mark(iter.next().unwrap(), boards));
-    let mut card = iter.next().unwrap();
-    mark(card, boards);
-
-    let mut found = boards.iter().position(|v| bingo(v));
-    while found.is_none() {
-        card = iter.next().unwrap();
-        mark(card, boards);
-        found = boards.iter().position(|v| bingo(v));
+    let board = &boards[i];
+    let mut sum = 0;
+    let mut unmarked = !marked[i] & 0x1FFFFFF;
+    while let Some(i) = bit(&mut unmarked) {
+        sum += board[i]
     }
-    let idx = found.unwrap();
-    let board = boards.get(idx).unwrap();
-    let unmarked: i32 = board.iter().filter(|t| !t.1).map(|t| t.0).sum();
-    let winner = unmarked * card;
-    boards.remove(idx);
 
-    let mut squid = 0;
-    for card in iter {
-        mark(card, boards);
-        while let Some(idx) = boards.iter().position(|v| bingo(v)) {
-            let board = boards.get(idx).unwrap();
-            let unmarked: i32 = board.iter().filter(|t| !t.1).map(|t| t.0).sum();
-            squid = unmarked * card;
-            boards.remove(idx);
+    n * sum
+}
+
+fn part_two(input: &str) -> u32
+{
+    let (nums, mut boards) = load(input);
+    let mut marks = vec![0;boards.len()];
+
+    let mut num    = 0;
+    let mut board  = vec![];
+    let mut marked = 0;
+    for n in nums {
+        mark(n, &boards, &mut marks);
+        while let Some(i) = bingo(&marks) {
+            num    = n;
+            board  = boards.remove(i);
+            marked = marks.remove(i);
         }
     }
 
-    (winner, squid)
+    let mut sum = 0;
+    let mut unmarked = !marked & 0x1FFFFFF;
+    while let Some(i) = bit(&mut unmarked) {
+        sum += board[i]
+    }
+
+    num * sum
 }
 
-fn mark(n: &i32, boards: &mut [Vec<(i32, bool)>]) {
-    boards.iter_mut().for_each(|b| {
-        b.iter_mut().for_each(|t| (t).1 |= t.0 == *n);
-    })
+static WINS: [u32;10] = [
+    0b0000000000000000000011111,
+    0b0000000000000001111100000,
+    0b0000000000111110000000000,
+    0b0000011111000000000000000,
+    0b1111100000000000000000000,
+    0b0000100001000010000100001,
+    0b0001000010000100001000010,
+    0b0010000100001000010000100,
+    0b0100001000010000100001000,
+    0b1000010000100001000010000,
+];
+
+fn mark(num: u32, boards: &[Vec<u32>], marked: &mut [u32])
+{
+    boards.iter()
+        .enumerate()
+        .flat_map(|(i, b)| b.iter().position(|n| *n == num).map(|p| (i, p)))
+        .for_each(|(i, p)| marked[i] |= 1 << p);
 }
 
-fn bingo(tab: &[(i32, bool)]) -> bool {
-    tab.chunks(5).any(|row| row.iter().all(|v| v.1))
-        || (0..5).any(|col| get_col(col, tab).all(|v| v.1))
+fn bingo(marked: &[u32]) -> Option<usize>
+{
+    marked.iter()
+        .position(|n| WINS.iter().any(|&w| w & n == w))
 }
 
-fn get_col(col: usize, tab: &[(i32, bool)]) -> impl Iterator<Item = &(i32, bool)> {
-    tab.iter().skip(col).step_by(5)
+fn bit(n: &mut u32) -> Option<usize> {
+    match n {
+        0 => None,
+        _ => {
+            let i = n.trailing_zeros();
+            *n &= !(1 << i);
+            Some(i as usize)
+        }
+    }
 }
+
+fn load(input: &str) -> (Vec<u32>, Vec<Vec<u32>>)
+{
+    let mut it = input.split("\n\n");
+    let nums = it.next()
+        .map(|s| s.split(',')
+            .flat_map(|n| n.parse::<u32>())
+            .collect::<Vec<_>>()
+        )
+        .unwrap();
+
+    let boards = it
+        .map(|board| board.lines()
+            .fold(vec![], |mut v, line| {
+                v.extend(line.split_whitespace()
+                    .flat_map(|s| s.parse::<u32>()));
+                v
+            })
+        )
+        .collect::<Vec<_>>();
+
+    (nums, boards)
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let (cards, mut boards) = load(include_str!("../input.txt"));
-
-        let (winner, squid) = doit(&cards, &mut boards);
-        assert_eq!(winner, 58838);
-        assert_eq!(squid, 6256);
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 58838);
     }
+
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 6256);
+    }
+
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one(input), 4512);
+    }
+
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_two(input), 1924);
+    }
+
 }

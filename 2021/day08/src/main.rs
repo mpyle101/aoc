e310dad819a7ef51c29e362b2dafd8c936fa4647
Michael Pyle <mpyle101@gmail.com>
2025-01-01 19:25:58 -0700

    2021 Day08 Redux

--- 2021/day08/src/main.rs
+++ 2021/day08/src/main.rs
@@ -1,137 +1,136 @@
-use std::collections::{ HashSet, HashMap };
+use std::collections::{HashMap, HashSet};
 
-fn main() {
-    use std::fs;
+fn main()
+{
     use std::time::Instant;
-    
-    let input   = fs::read_to_string("./input.txt").unwrap();
-    let signals = load(&input);
 
-    let t1 = Instant::now();
-    let count = part_one(&signals);
-    let t2 = Instant::now();
-    println!("Part 1: {} {:?}", count, t2 - t1);
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let total = part_two(&signals);
-    let t2 = Instant::now();
-    println!("Part 2: {} {:?}", total, t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn load(input: &str) -> Vec<(Vec<&str>, Vec<&str>)> {
-    input.lines().map(|l| {
-        let v = l.split(" | ").collect::<Vec<_>>();
-        (split(v[0]), split(v[1]))
-    }).collect()
+fn part_one(input: &str) -> usize
+{
+    let check = |&n: &usize| n == 2 || n == 3 || n == 4 || n == 7;
+
+    input.lines()
+        .flat_map(|line| line.split_once(" | "))
+        .map(|(_, s)| s.split(' '))
+        .map(|sp| sp.map(|s| s.len()).filter(check).count())
+        .sum()
 }
 
-fn part_one(signals: &[(Vec<&str>, Vec<&str>)]) -> i32 {
-    signals.iter().map(|(_, outputs)| {
-        outputs.iter().filter(|v|
-            v.len() == 2 || v.len() == 3 || v.len() == 4 || v.len() == 7
-        ).count() as i32
-    }).sum()
-}
-
-fn part_two(signals: &[(Vec<&str>, Vec<&str>)]) -> i32 {
-    let digits = HashMap::from([
-        (String::from("cf"),      '1'),
-        (String::from("acf"),     '7'),
-        (String::from("bcdf"),    '4'),
-        (String::from("acdeg"),   '2'),
-        (String::from("acdfg"),   '3'),
-        (String::from("abdfg"),   '5'),
-        (String::from("abcefg"),  '0'),
-        (String::from("abdefg"),  '6'),
-        (String::from("abcdfg"),  '9'),
-        (String::from("abcdefg"), '8'),
+fn part_two(input: &str) -> u32
+{
+    let to_char = |set: HashSet<char>| *set.iter().next().unwrap();
+    let digits: HashMap<String, u32> = HashMap::from([
+        ("abcefg".into(),  0),
+        ("cf".into(),      1),
+        ("acdeg".into(),   2),
+        ("acdfg".into(),   3),
+        ("bcdf".into(),    4),
+        ("abdfg".into(),   5),
+        ("abdefg".into(),  6),
+        ("acf".into(),     7),
+        ("abcdefg".into(), 8),
+        ("abcdfg".into(),  9),
     ]);
 
-    signals.iter().map(|(wires, outputs)| {
-        let one   = find_by_length(wires, 2);
-        let four  = find_by_length(wires, 4);
-        let seven = find_by_length(wires, 3);
-        let eight = find_by_length(wires, 7);
+    input.lines()
+        .flat_map(|line| line.split_once(" | "))
+        .map(|(s1, s2)| (s1.split(' '), s2.split(' ')))
+        .map(|(sp1, sp2)| {
+            // Note, each line contains all ten unique signal patterns
+            // on the left side.
+            let v = segments(sp1);
+            let cf      = &v[2][0];     // 1
+            let acf     = &v[3][0];     // 7
+            let bcdf    = &v[4][0];     // 4
+            let abcdefg = &v[7][0];     // 8
 
-        let abcdf = &four | &seven;
-        let fives = filter_by_length(wires, 5);
-        let two   = fives.iter()
-            .filter(|v| (*v - &abcdf).len() == 2)
-            .collect::<Vec<_>>();
-        let tmp   = fives.iter()
-            .filter(|v| (*v - &abcdf).len() == 1)
-            .collect::<Vec<_>>();
-        let two   = *two.first().unwrap();
-        let tmp   = *tmp.first().unwrap();
+            let a      = acf - cf;
+            let bd     = bcdf - cf;
+            let abcdf  = bcdf | &a;
+            let acdeg  = v[5].iter().find(|&s| (s - &abcdf).len() == 2).unwrap();
+            let abcdfg = v[6].iter().find(|&s| (s - &abcdf).len() == 1).unwrap();
+            let b      = &bd - acdeg;
+            let d      = &bd - &b;
+            let e      = abcdefg - abcdfg;
+            let f      = &abcdf - &(&bd | acdeg);
+            let g      = abcdfg - &abcdf;
+            let c      = cf - &f;
 
-        let bd    = &four - &one;
-        let b     = &bd - two;
-        let d     = &bd - &b;
-        let g     = tmp - &abcdf;
-        let f     = &abcdf - &(two | &bd);
-        let a     = &seven - &one;
-        let nine  = &(&four | &a) | &g;
-        let e     = &eight - &nine;
-        let c     = &one - &f;
+            let keys = HashMap::from([
+                (to_char(a), 'a'),
+                (to_char(b), 'b'),
+                (to_char(c), 'c'),
+                (to_char(d), 'd'),
+                (to_char(e), 'e'),
+                (to_char(f), 'f'),
+                (to_char(g), 'g'),
+            ]);
 
-        let key = HashMap::from([
-            (as_char(&a), 'a'),
-            (as_char(&b), 'b'),
-            (as_char(&c), 'c'),
-            (as_char(&d), 'd'),
-            (as_char(&e), 'e'),
-            (as_char(&f), 'f'),
-            (as_char(&g), 'g'),
-        ]);
-
-        let v = outputs.iter()
-            .map(|s| decode(&key, s))
-            .map(|s| digits.get(&s).unwrap())
-            .collect::<String>();
-        v.parse::<i32>().unwrap()
-    }).sum()
+            sp2.map(|s| {
+                    let mut v = s.chars()
+                        .flat_map(|c| keys.get(&c))
+                        .collect::<Vec<_>>();
+                    v.sort();
+                    v.iter().cloned().collect::<String>()
+                })
+                .zip((0..=3).rev())
+                .fold(0, |acc, (s, i)| {
+                    let n = digits.get(&s).unwrap();
+                    acc + (n * 10u32.pow(i))
+                })
+        })
+        .sum()
 }
 
-fn split(line: &str) -> Vec<&str> {
-    line.split(' ').collect::<Vec<_>>()
-}
-
-fn decode(key: &HashMap<char, char>, s: &str) -> String {
-    let mut chars = s.chars().map(|c| *key.get(&c).unwrap()).collect::<Vec<char>>();
-    chars.sort_unstable();
-    String::from_iter(chars)
-}
-
-fn find_by_length(wires: &[&str], len: usize) -> HashSet<char> {
-    wires.iter().find(|s| s.len() == len).unwrap().chars().collect()
-}
-
-fn filter_by_length(wires: &[&str], len: usize) -> Vec<HashSet<char>> {
-    wires.iter()
-        .filter(|s| s.len() == len)
-        .map(|s| s.chars().collect::<HashSet<_>>())
-        .collect()
-}
-
-fn as_char(set: &HashSet<char>) -> char {
-    *set.iter().next().unwrap()
+type Segments = Vec<Vec<HashSet<char>>>;
+fn segments<'a>(it: impl Iterator<Item=&'a str>) -> Segments
+{
+    let mut segments = vec![vec![];8];
+    it.map(|s| s.chars().collect::<HashSet<_>>())
+        .for_each(|s| segments[s.len()].push(s) );
+    segments
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs;
 
     #[test]
-    fn it_works() {
-        let input   = fs::read_to_string("./input.txt").unwrap();
-        let signals = load(&input);
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 381);
+    }
 
-        let count = part_one(&signals);
-        assert_eq!(count, 381);
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 1023686);
+    }
 
-        let total = part_two(&signals);
-        assert_eq!(total, 1023686);
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one(input), 26);
+    }
+
+    #[test]
+    fn example_part_two()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_two(input), 61229);
     }
 }

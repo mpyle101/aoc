7ff17b59fd239f320ee077d0ad47464f6ecee5bb
Michael Pyle <mpyle101@gmail.com>
2025-01-04 20:48:33 -0700

    2016 Day13 Redux

--- 2016/day13/src/main.rs
+++ 2016/day13/src/main.rs
@@ -1,80 +1,71 @@
 use std::collections::HashMap;
 
-fn main() {
+type Memo = HashMap<(i32, i32), i32>;
+
+fn main()
+{
     use std::time::Instant;
 
-    let t1 = Instant::now();
-    let steps = part_one((31, 39), 1350);
-    let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", steps, t2 - t1);
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let nodes = part_two(1350);
-    let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", nodes, t2 - t1);
+    let t = Instant::now();
+    let result = part_one((31, 39), input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(goal: (i32, i32), input: i32) -> usize {
+fn part_one(goal: (i32, i32), input: &str) -> usize
+{
     use pathfinding::prelude::bfs;
 
-    let steps = bfs(&(1, 1), |p| neighbors(p, input), |&p| p == goal);
-
-    // The vector contains the initial state.
-    steps.unwrap().len() - 1
+    let num = input.parse::<i32>().unwrap();
+    bfs(&(1, 1), |p| neighbors(p, num), |&p| p == goal)
+        .unwrap()
+        .len() - 1  // the vector contains the initial state.
 }
 
-fn part_two(input: i32) -> usize {
+fn part_two(input: &str) -> usize
+{
     use pathfinding::prelude::bfs_reach;
 
-    let mut steps = HashMap::from([((1, 1), 0)]);
-    let nodes = bfs_reach((1, 1), |p| reachable(p, &mut steps, input));
-
-    // The vector contains the initial state.
-    nodes.count()
+    let num = input.parse::<i32>().unwrap();
+    let mut memo = Memo::new();
+    bfs_reach((1, 1), |p| reachable(p, num, &mut memo)).count()
 }
 
-fn neighbors((x, y): &(i32, i32), input: i32) -> Vec<(i32, i32)> {
-    [(0, -1), (0, 1), (-1, 0), (1, 0)].iter()
-        .filter_map(|(dx, dy)| {
-            let (px, py) = (x + dx, y + dy);
-            if px >= 0 && py >= 0 && is_open(px, py, input) {
-                Some((px, py))
-            } else {
-                None
-            }
-        })
+fn neighbors((x, y): &(i32, i32), num: i32) -> Vec<(i32, i32)>
+{
+    [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        .iter()
+        .map(|(dx, dy)| (x + dx, y + dy))
+        .filter(|(x, y)| *x >= 0 && *y >= 0)
+        .filter(|p| is_open(*p, num))
         .collect()
 }
 
-fn reachable(
-    (x, y): &(i32, i32),
-    steps: &mut HashMap<(i32, i32), i32>,
-    input: i32,
-) -> Vec<(i32, i32)> {
-    let n = *steps.get(&(*x, *y)).unwrap();
-
-    [(0, -1), (0, 1), (-1, 0), (1, 0)].iter()
-        .filter_map(|(dx, dy)| {
-            let (px, py) = (x + dx, y + dy);
-            if px >= 0 && py >= 0 && n < 50 && is_open(px, py, input) {
-                steps.insert((px, py), n + 1);
-                Some((px, py))
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-fn is_open(x: i32, y: i32, input: i32) -> bool {
-    let mut ones = 0;
-    let mut n = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + input;
-    while n > 0 {
-        if n % 2 != 0 { ones += 1 }
-        n /= 2
+fn reachable((x, y): &(i32, i32), num: i32, memo: &mut Memo) -> Vec<(i32, i32)>
+{
+    let n = *memo.entry((*x, *y)).or_default();
+    if n < 50 {
+        [(0, -1), (0, 1), (-1, 0), (1, 0)]
+            .iter()
+            .map(|(dx, dy)| (x + dx, y + dy))
+            .filter(|(x, y)| *x >= 0 && *y >= 0)
+            .filter(|p| is_open(*p, num))
+            .inspect(|p| { memo.insert(*p, n + 1); })
+            .collect()
+    } else {
+        vec![]
     }
+}
 
-    ones % 2 == 0
+fn is_open((x, y): (i32, i32), num: i32) -> bool
+{
+    let n = x.pow(2) + (3 * x) + (2 * x * y) + y + y.pow(2) + num;
+    n.count_ones() % 2 == 0
 }
 
 
@@ -83,17 +74,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let steps = part_one((31, 39), 1350);
-        assert_eq!(steps, 92);
-
-        let steps = part_two(1350);
-        assert_eq!(steps, 124);
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one((31, 39), input), 92);
     }
 
     #[test]
-    fn samples() {
-        let steps = part_one((7, 4), 10);
-        assert_eq!(steps, 11);
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 124);
+    }
+
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one((7, 4), input), 11);
     }
 }

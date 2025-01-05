36290ee70d286cc0f00dcd58d8dd99cbf2e5f336
Michael Pyle <mpyle101@gmail.com>
2025-01-05 13:08:15 -0700

    2019 Day04 Redux

--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -1,146 +1,90 @@
-// Break the password value into an array of bytes containing
-// the digits from left to right.
-// Run through digits applying the rules: non increasing and at
-// least one pair+ for Part 1.
-// Non increasing and one pair for Part 2 (keep track of digit
-// runs and mark the password valid if a run of length 2 is found).
+fn main()
+{
+    use std::time::Instant;
 
-use std::cell::RefCell;
+    let input = include_str!("../input.txt");
 
-fn main() {
-  let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-  let count = (109165..=576723).map(|v| make_password(v, &buf))
-    .filter(|&p| check_password2(p))
-    .count();
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
 
-  println!("{count} valid passwords");
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn make_password(v: u32, rc: &RefCell<[u8; 6]>) -> &RefCell<[u8; 6]> {
-  let mut buf = rc.borrow_mut();
-  let mut num = v;
-  let mut idx: i8 = 5;
-  while num > 0 {
-    buf[idx as usize] = (num % 10) as u8;
-    num /= 10;
-    idx -= 1;
-  }
-  rc
+fn part_one(input: &str) -> usize
+{
+    let (s1, s2) = input.split_once('-').unwrap();
+    let n1 = s1.parse::<u32>().unwrap();
+    let n2 = s2.parse::<u32>().unwrap();
+
+    (n1..n2)
+        .map(digits)
+        .filter(|d| d.windows(2).all(|w| w[0] <= w[1]))
+        .filter(|d| d.windows(2).any(|w| w[0] == w[1]))
+        .count()
 }
 
-#[allow(dead_code)]
-fn check_password1(rc: &RefCell<[u8; 6]>) -> bool {
-  let passwd = rc.borrow();
-  let mut last = passwd[0];
-  let mut valid = false;
-  for digit in passwd.iter().skip(1) {
-    match *digit {
-      d if d < last  => return false,
-      d if d == last => valid = true,
-      _ => ()
+fn part_two(input: &str) -> usize
+{
+    let (s1, s2) = input.split_once('-').unwrap();
+    let n1 = s1.parse::<u32>().unwrap();
+    let n2 = s2.parse::<u32>().unwrap();
+
+    (n1..n2)
+        .map(digits)
+        .filter(repeat)
+        .filter(|d| d.windows(2).all(|w| w[0] <= w[1]))
+        .count()
+}
+
+fn digits(mut val: u32) -> [u32;6]
+{
+    let mut i = 5;
+    let mut arr = [0;6];
+    while val > 0 {
+        arr[i] = val % 10;
+        val /= 10;
+        i -= 1
     }
-    last = *digit
-  }
 
-  valid
+    arr
 }
 
-fn check_password2(rc: &RefCell<[u8; 6]>) -> bool {
-  let passwd = rc.borrow();
-  let mut run = 1;
-  let mut last = passwd[0];
-  let mut valid = false;
-  for digit in passwd.iter().skip(1) {
-    match *digit {
-      d if d < last  => return false,
-      d if d == last => run += 1,
-      _ => {
-        if run == 2 { valid = true }
-        run = 1;
-      }
+fn repeat(digits: &[u32;6]) -> bool
+{
+    let mut count = 1;
+    for w in digits.windows(2) {
+        if w[1] != w[0] {
+            if count == 2 {
+                return true
+            }
+            count = 1;
+        } else {
+            count += 1
+        }
     }
-    last = *digit;
-  }
 
-  valid || run == 2
+    count == 2
 }
 
 
-/** Unit Tests */
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn can_make_passwords() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(109165, &buf);
-    let expected: [u8; 6] = [1,0,9,1,6,5];
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 2814);
+    }
 
-    assert_eq!(*digits.borrow(), expected);
-  }
-
-  #[test]
-  fn one_valid_password1() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(111111, &buf);
-
-    assert!(check_password1(digits));
-  }
-
-  #[test]
-  fn one_valid_password2() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(223459, &buf);
-
-    assert!(check_password1(digits));
-  }
-
-  #[test]
-  fn one_non_increasing_fails() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(223450, &buf);
-
-    assert!(!check_password1(digits));
-  }
-
-  #[test]
-  fn one_no_double_digit_fails() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(123789, &buf);
-
-    assert!(!check_password1(digits));
-  }
-
-  #[test]
-  fn two_valid_password1() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(112233, &buf);
-
-    assert!(check_password2(digits));
-  }
-
-  #[test]
-  fn two_valid_password2() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(567899, &buf);
-
-    assert!(check_password2(digits));
-  }
-
-  #[test]
-  fn two_valid_password3() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(111122, &buf);
-
-    assert!(check_password2(digits));
-  }
-
-  #[test]
-  fn two_triple_fails() {
-    let buf: RefCell<[u8; 6]> = RefCell::new([0; 6]);
-    let digits = make_password(123444, &buf);
-
-    assert!(!check_password2(digits));
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 1991);
+    }
 }

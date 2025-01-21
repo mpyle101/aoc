b288c1a99866922270801afb3e0b426392c257ca
Michael Pyle <mpyle101@gmail.com>
2025-01-21 15:51:28 -0700

    Trade memory for speed

--- 2015/day20/src/main.rs
+++ 2015/day20/src/main.rs
@@ -1,78 +1,68 @@
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
-    let t1 = Instant::now();
-    let house = part_one(34000000);
-    let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", house, t2 - t1);
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let house = part_two(34000000);
-    let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", house, t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(presents: i32) -> i32 {
-    let mut house = 0;
+fn part_one(input: &str) -> usize
+{
+    let presents = input.parse::<usize>().unwrap();
 
-    loop {
-        house += 1;
-        let end = (f64::from(house).sqrt() as i32) + 1;
-        let count = (2..end).fold(house * 10 + 10, |acc, n|
-            if house % n == 0 {
-                acc + (n * 10) + (house / n * 10)
-            } else {
-                acc
-            }
-        );
-        if count >= presents {
-            break house
-        }
-    }
+    let last = presents / 10;
+    let mut v = vec![0_usize;last];
+
+    (1..last).for_each(|i| {
+        (i..last).step_by(i)
+            .for_each(|elf| v[elf] += i * 10);
+    });
+    
+    v.iter().position(|n| *n >= presents).unwrap()
 }
 
-fn part_two(presents: i32) -> i32 {
-    use std::collections::HashMap;
 
-    let mut active = HashMap::new();
-    let mut house = 0;
-    loop {
-        house += 1;
-        active.insert(house, 1);
-        let end = (f64::from(house).sqrt() as i32) + 1;
-        let count = (2..end).fold(house * 11 + 11, |mut acc, n| {
-            if house % n == 0 {
-                if let Some(visits) = active.get_mut(&n) {
-                    acc += n * 11;
-                    *visits += 1;
-                    if *visits == 50 { active.remove(&n); }
-                }
-                let v = house / n;
-                if let Some(visits) = active.get_mut(&v) {
-                    acc += v * 11;
-                    *visits += 1;
-                    if *visits == 50 { active.remove(&v); }
-                }
-            };
+fn part_two(input: &str) -> usize
+{
+    use std::cmp::min;
 
-            acc
-        });
-        if count >= presents {
-            break house
-        }
-    }
+    let presents = input.parse::<usize>().unwrap();
+
+    let last = presents / 10;
+    let mut v = vec![0_usize;last];
+
+    (1..last).for_each(|i| {
+        let n = min(i*50, last);
+        (i..n).step_by(i)
+            .for_each(|elf| v[elf] += i * 11);
+    });
+    
+    v.iter().position(|n| *n >= presents).unwrap()
 }
+
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let house = part_one(34000000);
-    assert_eq!(house, 786240);
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 786240);
+    }
 
-    let house = part_two(34000000);
-    assert_eq!(house, 831600);
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 831600);
+    }
 }

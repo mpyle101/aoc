cbdf3993f42f3ddab503276c72fdae4d85d8c7d4
Michael Pyle <mpyle101@gmail.com>
2025-01-20 21:49:28 -0700

    Restructure

--- 2015/day10/src/main.rs
+++ 2015/day10/src/main.rs
@@ -1,42 +1,53 @@
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
-    let t1 = Instant::now();
-    let len = part_one(&[3,1,1,3,3,2,2,1,1,3]);
-    let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", len, t2 - t1);
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let len = part_two(&[3,1,1,3,3,2,2,1,1,3]);
-    let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", len, t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(digits: &[u8]) -> usize {
-    (0..40).fold(digits.to_vec(), |v, _| cycle(&v)).len()
+fn part_one(input: &str) -> usize
+{
+    let digits = input.as_bytes().iter()
+        .map(|b| b - b'0')
+        .collect::<Vec<_>>();
+
+    (0..40).fold(digits, |v, _| cycle(&v)).len()
 }
 
-fn part_two(digits: &[u8]) -> usize {
-    (0..50).fold(digits.to_vec(), |v, _| cycle(&v)).len()
+fn part_two(input: &str) -> usize
+{
+    let digits = input.as_bytes().iter()
+        .map(|b| b - b'0')
+        .collect::<Vec<_>>();
+
+    (0..50).fold(digits, |v, _| cycle(&v)).len()
 }
 
-fn cycle(digits: &[u8]) -> Vec<u8> {
+fn cycle(digits: &[u8]) -> Vec<u8>
+{
     let mut run = 1;
     let mut curr = digits[0];
-
-    let mut v = digits.iter().skip(1).fold(
-        Vec::new(),
-        |mut v, &d| {
-        if d == curr { 
-            run += 1;
-        } else {
-            v.push(run);
-            v.push(curr);
-            run = 1;
-            curr = d;
-        };
-        v
-    });
+    let mut v = digits.iter()
+        .skip(1)
+        .fold(Vec::new(), |mut v, &d| {
+            if d == curr { 
+                run += 1;
+            } else {
+                v.push(run);
+                v.push(curr);
+                run  = 1;
+                curr = d;
+            };
+            v
+        });
     v.push(run);
     v.push(curr);
 
@@ -46,14 +57,19 @@ fn cycle(digits: &[u8]) -> Vec<u8> {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let len = part_one(&[3,1,1,3,3,2,2,1,1,3]);
-    assert_eq!(len, 329356);
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 329356);
+    }
 
-    let len = part_two(&[3,1,1,3,3,2,2,1,1,3]);
-    assert_eq!(len, 4666278);
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 4666278);
+    }
 }

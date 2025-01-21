6d14c4eb4f6162d14dd1237d1cbc42dc4505ae31
Michael Pyle <mpyle101@gmail.com>
2025-01-21 16:01:51 -0700

    Restructure

--- 2015/day21/src/main.rs
+++ 2015/day21/src/main.rs
@@ -1,5 +1,128 @@
 use lazy_static::lazy_static;
 
+fn main()
+{
+    use std::time::Instant;
+
+    let input = include_str!("../input.txt");
+
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
+}
+
+fn part_one(input: &str) -> i32
+{
+    use std::collections::HashMap;
+    use itertools::iproduct;
+
+    let boss = load(input);
+    let mut cache = HashMap::new();
+
+    iproduct!(
+        0..RINGS.len(),
+        0..RINGS.len(),
+        0..ARMOR.len(),
+        0..WEAPONS.len()
+    ).filter_map(|(r1, r2, a, w)| {
+        if r1 == r2 { 
+            None
+        } else {
+            let cost   = ARMOR[a].cost + WEAPONS[w].cost + RINGS[r1].cost + RINGS[r2].cost;
+            let armor  = ARMOR[a].armor + RINGS[r1].armor + RINGS[r2].armor;
+            let damage = WEAPONS[w].damage + RINGS[r1].damage + RINGS[r2].damage;
+            let player = Player { hp: 100, armor, damage };
+            let won = if let Some(result) = cache.get(&(armor, damage)) {
+                *result
+            } else {
+                let result = fight(&player, &boss);
+                cache.insert((armor, damage), result);
+                result
+            };
+            won.then_some(cost)
+        }
+    })
+    .min()
+    .unwrap()
+}
+
+fn part_two(input: &str) -> i32
+{
+    use std::collections::HashMap;
+    use itertools::iproduct;
+
+    let boss = load(input);
+    let mut cache = HashMap::new();
+
+    iproduct!(
+        0..RINGS.len(),
+        0..RINGS.len(),
+        0..ARMOR.len(),
+        0..WEAPONS.len()
+    ).filter_map(|(r1, r2, a, w)| {
+        if r1 == r2 { 
+            None
+        } else {
+            let cost   = ARMOR[a].cost + WEAPONS[w].cost + RINGS[r1].cost + RINGS[r2].cost;
+            let armor  = ARMOR[a].armor + RINGS[r1].armor + RINGS[r2].armor;
+            let damage = WEAPONS[w].damage + RINGS[r1].damage + RINGS[r2].damage;
+            let player = Player { hp: 100, armor, damage };
+            let won = if let Some(result) = cache.get(&(armor, damage)) {
+                *result
+            } else {
+                let result = fight(&player, &boss);
+                cache.insert((armor, damage), result);
+                result
+            };
+            (!won).then_some(cost)
+        }
+    })
+    .max()
+    .unwrap()
+}
+
+fn fight(player: &Player, boss: &Player) -> bool
+{
+    let players = [player, boss];
+    let mut hp  = [player.hp, boss.hp];
+
+    let mut p = 1;  // player goes first
+    loop {
+        let damage = players[1-p].damage - players[p].armor;
+        hp[p] -= if damage < 1 { 1 } else { damage };
+
+        // Return true if the player wins
+        if hp[p] < 1 { break p == 1 }
+
+        p = 1 - p;  // switch players
+    }
+}
+
+struct Player {
+    hp: i32,
+    armor: i32,
+    damage: i32,
+}
+
+fn load(input: &str) -> Player
+{
+    let mut it = input.lines()
+        .map(|l| {
+            let v = l.split(": ").collect::<Vec<_>>();
+            v[1].parse::<i32>().unwrap()
+        });
+    
+    Player {
+        hp:     it.next().unwrap(),
+        damage: it.next().unwrap(),
+        armor:  it.next().unwrap(),
+    }
+}
+
 struct Item {
     cost: i32,
     armor: i32,
@@ -7,7 +130,8 @@ struct Item {
 }
 
 impl Item {
-    fn new(cost: i32, damage: i32, armor: i32) -> Item {
+    fn new(cost: i32, damage: i32, armor: i32) -> Item
+    {
         Item { cost, armor, damage }
     }
 }
@@ -41,136 +165,22 @@ lazy_static! {
     ];
 }
 
-struct Player {
-    hp: i32,
-    armor: i32,
-    damage: i32,
-}
-
-fn main() {
-    use std::time::Instant;
-
-    let boss = load(include_str!("./input.txt"));
-
-    let t1 = Instant::now();
-    let cost = part_one(&boss);
-    let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", cost, t2 - t1);
-
-    let t1 = Instant::now();
-    let cost = part_two(&boss);
-    let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", cost, t2 - t1);
-}
-
-fn load(input: &str) -> Player {
-    let mut it = input.lines()
-        .map(|l| {
-            let v = l.split(": ").collect::<Vec<_>>();
-            v[1].parse::<i32>().unwrap()
-        });
-    
-    Player {
-        hp:     it.next().unwrap(),
-        damage: it.next().unwrap(),
-        armor:  it.next().unwrap(),
-    }
-}
-
-fn part_one(boss: &Player) -> i32 {
-    use std::collections::HashMap;
-    use itertools::iproduct;
-
-    let mut cache = HashMap::new();
-
-    iproduct!(
-        0..RINGS.len(),
-        0..RINGS.len(),
-        0..ARMOR.len(),
-        0..WEAPONS.len()
-    ).filter_map(|(r1, r2, a, w)| {
-        if r1 == r2 { 
-            None
-        } else {
-            let cost   = ARMOR[a].cost + WEAPONS[w].cost + RINGS[r1].cost + RINGS[r2].cost;
-            let armor  = ARMOR[a].armor + RINGS[r1].armor + RINGS[r2].armor;
-            let damage = WEAPONS[w].damage + RINGS[r1].damage + RINGS[r2].damage;
-            let player = Player { hp: 100, armor, damage };
-            let won = if let Some(result) = cache.get(&(armor, damage)) {
-                *result
-            } else {
-                let result = fight(&player, boss);
-                cache.insert((armor, damage), result);
-                result
-            };
-            won.then_some(cost)
-        }
-    })
-    .min()
-    .unwrap()
-}
-
-fn part_two(boss: &Player) -> i32 {
-    use std::collections::HashMap;
-    use itertools::iproduct;
-
-    let mut cache = HashMap::new();
-
-    iproduct!(
-        0..RINGS.len(),
-        0..RINGS.len(),
-        0..ARMOR.len(),
-        0..WEAPONS.len()
-    ).filter_map(|(r1, r2, a, w)| {
-        if r1 == r2 { 
-            None
-        } else {
-            let cost   = ARMOR[a].cost + WEAPONS[w].cost + RINGS[r1].cost + RINGS[r2].cost;
-            let armor  = ARMOR[a].armor + RINGS[r1].armor + RINGS[r2].armor;
-            let damage = WEAPONS[w].damage + RINGS[r1].damage + RINGS[r2].damage;
-            let player = Player { hp: 100, armor, damage };
-            let won = if let Some(result) = cache.get(&(armor, damage)) {
-                *result
-            } else {
-                let result = fight(&player, boss);
-                cache.insert((armor, damage), result);
-                result
-            };
-            (!won).then_some(cost)
-        }
-    })
-    .max()
-    .unwrap()
-}
-
-fn fight(player: &Player, boss: &Player) -> bool {
-    let players = [player, boss];
-    let mut hp  = [player.hp, boss.hp];
-
-    let mut p = 1;  // player goes first
-    loop {
-        let damage = players[1-p].damage - players[p].armor;
-        hp[p] -= if damage < 1 { 1 } else { damage };
-            
-        // Return true if the player wins
-        if hp[p] < 1 { break p == 1 }
-
-        p = 1 - p;  // switch players
-    }
-}
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let boss = load(include_str!("./input.txt"));
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 111);
+    }
 
-    let cost = part_one(&boss);
-    assert_eq!(cost, 111);
-
-    let cost = part_two(&boss);
-    assert_eq!(cost, 188);
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 188);
+    }
 }

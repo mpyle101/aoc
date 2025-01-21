73916ea57958b2dc8a55818829f4e127dd2482dd
Michael Pyle <mpyle101@gmail.com>
2025-01-20 22:00:11 -0700

    Restructure

--- 2015/day12/src/main.rs
+++ 2015/day12/src/main.rs
@@ -1,36 +1,38 @@
-use serde_json::{ Map, Value };
+use serde_json::{Map, Value};
 
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
-    let input = include_str!("./input.txt");
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let sum = part_one(input);
-    let t2 = Instant::now();
-    println!("Part 1: {sum} ({:?})", t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
 
-    let t1 = Instant::now();
-    let sum = part_two(input);
-    let t2 = Instant::now();
-    println!("Part 2: {sum} ({:?})", t2 - t1);
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(s: &str) -> i32 {
+fn part_one(input: &str) -> i32
+{
     use regex::Regex;
 
     let re = Regex::new(r"(?-u:\-?\d+)").unwrap();
-    re.find_iter(s)
-        .map(|v| v.as_str().parse::<i32>().unwrap())
+    re.find_iter(input)
+        .flat_map(|v| v.as_str().parse::<i32>())
         .sum()
 }
 
-fn part_two(s: &str) -> i64 {
-    let json: Vec<Value> = serde_json::from_str(s).unwrap();
+fn part_two(input: &str) -> i64
+{
+    let json: Vec<Value> = serde_json::from_str(input).unwrap();
     json.iter().map(process).sum()
 }
 
-fn process(v: &Value) -> i64 {
+fn process(v: &Value) -> i64
+{
     match v {
         Value::Number(n)   => n.as_i64().unwrap(),
         Value::Array(arr)  => arr.iter().map(process).sum(),
@@ -39,23 +41,28 @@ fn process(v: &Value) -> i64 {
     }
 }
 
-fn process_attributes(obj: &Map<String, Value>) -> i64 {
+fn process_attributes(obj: &Map<String, Value>) -> i64
+{
     let red = obj.values().any(|v| v.as_str() == Some("red"));
     if red { 0 } else { obj.values().map(process).sum() }
 }
 
+
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let input = include_str!("./input.txt");
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 191164);
+    }
 
-    let sum = part_one(input);
-    assert_eq!(sum, 191164);
-
-    let sum = part_two(input);
-    assert_eq!(sum, 87842);
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 87842);
+    }
 }

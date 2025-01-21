1fdc843059db53c2b5ec795aa7506ffd102dc413
Michael Pyle <mpyle101@gmail.com>
2025-01-20 22:10:30 -0700

    Restructure

--- 2015/day17/src/main.rs
+++ 2015/day17/src/main.rs
@@ -1,59 +1,74 @@
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
-    let containers = load(include_str!("./input.txt"));
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let count = part_one(&containers);
-    let t2 = Instant::now();
-    println!("Part 1: {} ({:?})", count, t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
 
-    let t1 = Instant::now();
-    let count = part_two(&containers);
-    let t2 = Instant::now();
-    println!("Part 2: {} ({:?})", count, t2 - t1);
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn load(input: &str) -> Vec<i32> {
-    input.lines().map(|s| s.parse::<i32>().unwrap()).collect()
-}
-
-fn part_one(containers: &[i32]) -> i32 {
+fn part_one(input: &str) -> usize
+{
     use itertools::Itertools;
 
-    (4..containers.len()).flat_map(|n|
-        containers.iter().combinations(n)
+    let containers = load(input);
+    (4..containers.len())
+        .flat_map(|n| containers.iter()
+            .combinations(n)
             .filter(|v| v.iter().cloned().sum::<i32>() == 150)
-    ).count() as i32
+        )
+        .count()
 }
 
-fn part_two(containers: &[i32]) -> i32 {
+fn part_two(input: &str) -> usize
+{
     use itertools::Itertools;
 
-    let mut seqs = (4..containers.len()).flat_map(|n|
-        containers.iter().combinations(n)
+    let containers = load(input);
+    let mut seqs = (4..containers.len())
+        .flat_map(|n| containers.iter()
+            .combinations(n)
             .filter(|v| v.iter().cloned().sum::<i32>() == 150)
             .map(|v| v.len())
             .collect::<Vec<_>>()
-    ).collect::<Vec<_>>();
+        )
+        .collect::<Vec<_>>();
     seqs.sort_unstable();
 
-    seqs.iter().filter(|&n| *n == seqs[0]).count() as i32
+    seqs.iter()
+        .filter(|&n| *n == seqs[0])
+        .count()
+}
+
+fn load(input: &str) -> Vec<i32>
+{
+    input.lines()
+        .flat_map(|s| s.parse::<i32>())
+        .collect()
 }
 
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let containers = load(include_str!("./input.txt"));
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 4372);
+    }
 
-    let count = part_one(&containers);
-    assert_eq!(count, 4372);
-
-    let count = part_two(&containers);
-    assert_eq!(count, 4);
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 4);
+    }
 }

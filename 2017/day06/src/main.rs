65e63c2cda7364ea6fb710a8d486cf76136abf03
Michael Pyle <mpyle101@gmail.com>
2025-01-22 15:57:09 -0700

    A little futzing

--- 2017/day06/src/main.rs
+++ 2017/day06/src/main.rs
@@ -1,91 +1,88 @@
-
-fn main() {
-    use std::fs;
+fn main()
+{
     use std::time::Instant;
-    
-    let input  = fs::read_to_string("./input.txt").unwrap();
-    let blocks = load(&input);
 
-    let t1 = Instant::now();
-    let cycles = part_one(&blocks);
-    let t2 = Instant::now();
-    println!("Part 1: {} {:?}", cycles, t2 - t1);
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let cycles = part_two(&blocks);
-    let t2 = Instant::now();
-    println!("Part 2: {} {:?}", cycles, t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn load(input: &str) -> Vec<i32> {
-    input.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect()
-}
-
-fn part_one(blocks: &[i32]) -> i32 {
+fn part_one(input: &str) -> i32
+{
     use std::collections::HashSet;
 
-    let mut banks  = blocks.to_vec();
+    let mut banks  = load(input);
     let mut states = HashSet::new();
-    while !states.contains(&banks) {
-        states.insert(banks.clone());
-        banks = cycle(&banks);
+    while states.insert(banks.clone()) {
+        cycle(&mut banks);
     }
 
     states.len() as i32
 }
 
-fn part_two(blocks: &[i32]) -> i32 {
+fn part_two(input: &str) -> i32
+{
     use std::collections::HashSet;
 
-    let mut banks  = blocks.to_vec();
+    let mut banks  = load(input);
     let mut states = HashSet::new();
-    while !states.contains(&banks) {
-        states.insert(banks.clone());
-        banks = cycle(&banks);
+    while states.insert(banks.clone()) {
+        cycle(&mut banks);
     }
 
     let state = banks.clone();
     let mut cycles = 1;
-    banks = cycle(&banks);
+    cycle(&mut banks);
     while banks != state { 
-        banks = cycle(&banks);
+        cycle(&mut banks);
         cycles += 1;
     }
 
     cycles
 }
 
+fn load(input: &str) -> Vec<i32>
+{
+    input.split_whitespace()
+        .flat_map(|s| s.parse::<i32>())
+        .collect()
+}
 
-fn cycle(banks: &[i32]) -> Vec<i32> {
+fn cycle(banks: &mut [i32])
+{
     let mut n = *banks.iter().max().unwrap();
     let mut i = banks.iter().position(|v| *v == n).unwrap();
 
-    let mut blocks = banks.to_vec();
-    blocks[i] = 0;
+    banks[i] = 0;
     while n > 0 {
         i = (i + 1) % banks.len();
-        blocks[i] += 1;
+        banks[i] += 1;
         n -= 1;
     }
-    
-    blocks
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs;
 
     #[test]
-    fn it_works() {
-        let input  = fs::read_to_string("./input.txt").unwrap();
-        let blocks = load(&input);
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 11137);
+    }
 
-        let cycles = part_one(&blocks);
-        assert_eq!(cycles, 11137);
-
-        let cycles = part_two(&blocks);
-        assert_eq!(cycles, 1037);
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 1037);
     }
 }

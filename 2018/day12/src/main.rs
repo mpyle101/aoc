3a3c91c2a3e8aa94aed8d6aa8b3717263afb76a7
Michael Pyle <mpyle101@gmail.com>
2025-01-24 14:59:53 -0700

    Way more understandable

--- 2018/day12/src/main.rs
+++ 2018/day12/src/main.rs
@@ -1,81 +1,102 @@
-use std::collections::{BTreeSet, HashMap};
+use std::collections::BTreeSet;
 
-type Rules<'a> = HashMap<&'a [u8], u8>;
 type State = BTreeSet<i32>;
+type Rules = Vec<(u8, u8)>;
 
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
-    let (state, rules) = load(include_str!("./input.txt"));
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let plants = part_one(&state, &rules);
-    let t2 = Instant::now();
-    println!("Part 1: {}  ({:?})", plants, t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
 
-    let t1 = Instant::now();
-    let plants = part_two();
-    let t2 = Instant::now();
-    println!("Part 2: {}  ({:?})", plants, t2 - t1);
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(initial_state: &State, rules: &Rules) -> i32 {
-    let state = cycle(20, initial_state, rules);
+fn part_one(input: &str) -> i32
+{
+    let (state, rules) = load(input);
+    let state = (0..20).fold(state, |st, _| cycle(&st, &rules));
     state.iter().sum()
 }
 
-fn part_two() -> i64 {
-    // via inspection of the repeating value delta pattern showing up
-    // around 160 generations or so.
-    (((50_000_000_000 - 200) / 20) * 1500) + 16113
+fn part_two(input: &str) -> i64
+{
+    // Via inspection, generation 169 and after are all 75
+    // more than the previous generation. So, we get the sum
+    // at generation 168 and add 75 to it for each generation
+    // after that.
+
+    let (state, rules) = load(input);
+    let st = (0..=168).fold(state.clone(), |st, _| {
+        cycle(&st, &rules)
+    });
+    let start = st.iter().sum::<i32>() as i64;
+
+    let n = 50_000_000_000 - 169;
+    start + n * 75
 }
 
-fn cycle(generations: u64, initial_state: &State, rules: &Rules) -> State {
-    let mut pat: [u8;5] = [b'.';5];
-    let mut state: State = initial_state.iter().cloned().collect();
-    for _ in 0..generations {
-        let mut genx = State::new();
+fn cycle(state: &State, rules: &Rules) -> State
+{
+    use std::collections::HashSet;
 
-        let mut i = state.iter().next().unwrap() - 4;
-        pat[0] = if state.contains(&(i)) { b'#' } else { b'.' };
-        pat[1] = if state.contains(&(i+1)) { b'#' } else { b'.' };
-        pat[2] = if state.contains(&(i+2)) { b'#' } else { b'.' };
-        pat[3] = if state.contains(&(i+3)) { b'#' } else { b'.' };
-        pat[4] = if state.contains(&(i+4)) { b'#' } else { b'.' };
+    // Because we have a rule whereby ..... => . we only need to deal
+    // with chunks that have plants in them. But we do need to check
+    // two plots to the left and two to the right of each potted plant
+    // because it may cause one of them to grow or die.
+    let mut checked = HashSet::new();
 
-        let end = state.iter().next_back().unwrap() + 1;
-        for _ in i+1..end {
-            if let Some(&b) = rules.get(&pat as &[u8]) {
-                if b == b'#' {
-                    genx.insert(i+2);
-                }
-            }
-
-            i += 1;
-            pat[0] = pat[1];
-            pat[1] = pat[2];
-            pat[2] = pat[3];
-            pat[3] = pat[4];
-            pat[4] = if state.contains(&(i+4)) { b'#' } else { b'.' };
-        }
-        state = genx;
-    }
-    
-    state
+    state.iter()
+        .fold(State::new(), |mut st, n| {
+            (n-2..=n+2)
+                .filter(|n| checked.insert(*n))
+                .filter_map(|n| check(n, state, rules).map(|v| (n, *v)))
+                .filter(|(_, v)| *v == 1)
+                .for_each(|(n, _)| { st.insert(n); });
+            st
+        })
 }
 
-fn load(input: &str) -> (State, Rules) {
-    let mut iter = input.lines();
-    let state: State = iter.next().unwrap()
-        .split(' ').collect::<Vec<_>>()[2].as_bytes().iter()
+fn check<'a>(n: i32, state: &State, rules: &'a Rules) -> Option<&'a u8>
+{
+    let pattern = (n-2..=n+2)
         .enumerate()
-        .filter(|(_, &b)| b == b'#')
-        .map(|(i, _)| i as i32)
-        .collect();
-    iter.next();
-    let rules: Rules = iter.map(|s| s.split(' ').collect::<Vec<_>>())
-        .map(|v| (v[0].as_bytes(), v[2].as_bytes()[0]))
-        .collect();
+        .filter(|(_, n)| state.contains(n))
+        .fold(0u8, |p, (i, _)| p | 1 << (4 - i));
+
+    rules.iter().
+        find(|(p, _)| *p == pattern)
+        .map(|(_, v)| v)
+}
+
+fn load(input: &str) -> (State, Rules)
+{
+    let (s1, s2) = input.split_once("\n\n").unwrap();
+
+    let (_, st) = s1.split_once(": ").unwrap();
+    let state = st.bytes()
+        .enumerate()
+        .filter_map(|(i, b)| (b == b'#').then_some(i as i32))
+        .collect::<BTreeSet<_>>();
+
+    let rules = s2.lines()
+        .flat_map(|line| line.split_once(" => "))
+        .map(|(s1, s2)| {
+            let pattern = s1.bytes()
+                .enumerate()
+                .filter(|(_, b)| *b == b'#')
+                .fold(0, |n, (i, _)| n | 1 << (4 - i) );
+            let plant = if s2.as_bytes()[0] == b'#' { 1 } else { 0 };
+
+            (pattern, plant)
+        })
+        .collect::<Vec<_>>();
 
     (state, rules)
 }
@@ -83,16 +104,26 @@ fn load(input: &str) -> (State, Rules) {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let (state, rules) = load(include_str!("./input.txt"));
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 3276);
+    }
 
-    let plants = part_one(&state, &rules);
-    assert_eq!(plants, 3276);
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 3750000001113);
+    }
 
-    let plants = part_two();
-    assert_eq!(plants, 3750000001113);
-  }
+    #[test]
+    fn example_part_one()
+    {
+        let input = include_str!("../example.txt");
+        assert_eq!(part_one(input), 325);
+    }
 }

84f8fb0f777d293e04d1fa8b9d5edf3b2c33071d
Michael Pyle <mpyle101@gmail.com>
2025-01-24 07:59:38 -0700

    Restructure

--- 2018/day11/src/main.rs
+++ 2018/day11/src/main.rs
@@ -1,20 +1,23 @@
 use ndarray::Array2;
 
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
-    let t1 = Instant::now();
-    let (x, y) = part_one(18);
-    let t2 = Instant::now();
-    println!("Part 1: {},{}  ({:?})", x, y, t2 - t1);
+    let input = include_str!("../input.txt");
 
-    let t1 = Instant::now();
-    let (x, y, d) = part_two(9424);
-    let t2 = Instant::now();
-    println!("Part 2: {},{},{}  ({:?})", x, y, d, t2 - t1);
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {:?} ({:?})", result, t.elapsed());
+
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {:?} ({:?})", result, t.elapsed());
 }
 
-fn part_one(serial_no: i32) -> (usize, usize) {
+fn part_one(input: &str) -> (usize, usize)
+{
+    let serial_no = input.parse::<i32>().unwrap();
     let mat = calc_sums(serial_no);
     let (_, x, y) = 
         (0..297).map(|x| 
@@ -26,7 +29,9 @@ fn part_one(serial_no: i32) -> (usize, usize) {
     (x + 1, y + 1)
 }
 
-fn part_two(serial_no: i32) -> (usize, usize, usize) {
+fn part_two(input: &str) -> (usize, usize, usize)
+{
+    let serial_no = input.parse::<i32>().unwrap();
     let mat = calc_sums(serial_no);
     let (_, x, y, d) = 
         (1..300).map(|d|
@@ -40,7 +45,8 @@ fn part_two(serial_no: i32) -> (usize, usize, usize) {
     (x + 1, y + 1, d)
 }
 
-fn sum_at(mat: &Array2<i32>, x: usize, y: usize, d: usize) -> i32 {
+fn sum_at(mat: &Array2<i32>, x: usize, y: usize, d: usize) -> i32
+{
     let r = x + (d - 1);
     let s = y + (d - 1);
     let rs = *mat.get((r, s)).unwrap();
@@ -51,7 +57,8 @@ fn sum_at(mat: &Array2<i32>, x: usize, y: usize, d: usize) -> i32 {
     rs - rq - ps + pq
 }
 
-fn calc_sums(serial_no: i32) -> Array2<i32> {
+fn calc_sums(serial_no: i32) -> Array2<i32>
+{
     let mut above = 0;
     let mut cache: [i32;300] = [0;300];
     let mut grid = Array2::<i32>::zeros((300, 300));
@@ -69,7 +76,8 @@ fn calc_sums(serial_no: i32) -> Array2<i32> {
 }
 
 #[inline]
-fn calc_power(serial_no: i32, x: usize, y: usize) -> i32 {
+fn calc_power(serial_no: i32, x: usize, y: usize) -> i32
+{
     let rid   = x as i32 + 10 + 1;
     let power = (rid * (y as i32 + 1) + serial_no) * rid;
     (if power < 100 { 0 } else { (power / 100) % 10 }) - 5
@@ -78,14 +86,19 @@ fn calc_power(serial_no: i32, x: usize, y: usize) -> i32 {
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let (x, y) = part_one(9424);
-    assert_eq!((x, y), (243, 72));
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), (243, 72));
+    }
 
-    let (x, y, d) = part_two(9424);
-    assert_eq!((x, y, d), (229,192,11));
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), (229,192,11));
+    }
 }

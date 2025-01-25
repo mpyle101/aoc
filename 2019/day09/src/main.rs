ccf9dd44983a96209a97ed93a625d7abb9d215b2
Michael Pyle <mpyle101@gmail.com>
2025-01-25 12:33:22 -0700

    Restructure

--- 2019/day09/src/main.rs
+++ 2019/day09/src/main.rs
@@ -1,40 +1,56 @@
 use vm::Vm;
 
-fn main() {
-  let program = include_str!("./boost.txt");
-  let mut vm = Vm::new(program).unwrap();
-  let (mut stdin, mut stdout) = vm.pipes();
+fn main()
+{
+    use std::time::Instant;
+    
+    let input = include_str!("../input.txt");
 
-  stdin.write(2);
-  vm.exec().unwrap();
+    let t = Instant::now();
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
 
-  println!("Distress co-ords {:?}", stdout.flush())
+    let t = Instant::now();
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
+}
+
+fn part_one(input: &str) ->i64
+{
+    let mut vm = Vm::new(input).unwrap();
+    let (mut stdin, mut stdout) = vm.pipes();
+
+    stdin.write(1);
+    vm.exec().unwrap();
+    stdout.flush()
+}
+
+fn part_two(input: &str) ->i64
+{
+    let mut vm = Vm::new(input).unwrap();
+    let (mut stdin, mut stdout) = vm.pipes();
+
+    stdin.write(2);
+    vm.exec().unwrap();
+    stdout.flush()
 }
 
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn it_works() {
-    let program = include_str!("./boost.txt");
-    let mut vm = Vm::new(program).unwrap();
-    let (mut stdin, mut stdout) = vm.pipes();
+    #[test]
+    fn input_part_one()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_one(input), 3013554615);
+    }
 
-    stdin.write(1);
-    assert_eq!(vm.exec().unwrap(), vm::State::Done);
-    assert_eq!(stdout.flush(), 3013554615);
-  }
-
-  #[test]
-  fn distress_coords() {
-    let program = include_str!("./boost.txt");
-    let mut vm = Vm::new(program).unwrap();
-    let (mut stdin, mut stdout) = vm.pipes();
-
-    stdin.write(2);
-    assert_eq!(vm.exec().unwrap(), vm::State::Done);
-    assert_eq!(stdout.flush(), 50158);
-  }
+    #[test]
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 50158);
+    }
 }

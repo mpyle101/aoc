7abba2f86fae8c7a36f09b14f5e2d0c6ed1e1a91
Michael Pyle <mpyle101@gmail.com>
2025-01-25 08:50:57 -0700

    Clean up

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -1,33 +1,36 @@
-
-fn main() {
+fn main()
+{
     use std::time::Instant;
 
     let input = include_str!("../input.txt");
 
     let t = Instant::now();
-    let fuel = part_one(input);
-    println!("Part 1: {} ({:?})", fuel, t.elapsed());
+    let result = part_one(input);
+    println!("Part 1: {} ({:?})", result, t.elapsed());
 
     let t = Instant::now();
-    let fuel = part_two(input);
-    println!("Part 2: {} ({:?})", fuel, t.elapsed());
+    let result = part_two(input);
+    println!("Part 2: {} ({:?})", result, t.elapsed());
 }
 
-fn part_one(input: &str) -> i32 {
+fn part_one(input: &str) -> i32
+{
     input.lines()
         .flat_map(|s| s.parse::<i32>())
         .map(|mass| mass / 3 - 2)
         .sum()
 }
 
-fn part_two(input: &str) -> i32 {
+fn part_two(input: &str) -> i32
+{
     input.lines()
         .flat_map(|s| s.parse::<i32>())
         .map(fuel_needed)
         .sum()
 }
 
-fn fuel_needed(mass: i32) -> i32 {
+fn fuel_needed(mass: i32) -> i32
+{
     let fuel = mass / 3 - 2;
     if fuel <= 0 { 0 } else { fuel + fuel_needed(fuel) }
 }
@@ -38,18 +41,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn input_part_one()
+    {
         let input = include_str!("../input.txt");
-
-        let fuel = part_one(input);
-        assert_eq!(fuel, 3317970);
-
-        let fuel = part_two(input);
-        assert_eq!(fuel, 4974073);
+        assert_eq!(part_one(input), 3317970);
     }
 
     #[test]
-    fn examples() {
+    fn input_part_two()
+    {
+        let input = include_str!("../input.txt");
+        assert_eq!(part_two(input), 4974073);
+    }
+
+    #[test]
+    fn example_part_one()
+    {
         assert_eq!(fuel_needed(12), 2);
         assert_eq!(fuel_needed(14), 2);
         assert_eq!(fuel_needed(1969), 966);

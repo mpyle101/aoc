0be5d3cc6bf607dc0f69f9de76745fea8435dba2
Michael Pyle <mpyle101@gmail.com>
2025-12-04 11:08:02 -0700

    Home grown matrix instead of pathfinding

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -1,5 +1,3 @@
-use pathfinding::matrix::Matrix;
-
 fn main()
 {
     use std::time::Instant;
@@ -17,14 +15,18 @@ fn main()
 
 fn part_one(input: &str) -> usize
 {
-    use pathfinding::matrix::Matrix;
+    let mut nrows = 0;
+    let mut ncols = 0;
+    let v = input.lines()
+        .inspect(|l| { nrows += 1; ncols = l.len() })
+        .fold(Vec::new(), |mut v, l| { v.extend_from_slice(l.as_bytes()); v });
 
-    let m = Matrix::from_rows(input.lines().map(|l| l.bytes())).unwrap();
-    m.items()
+    v.iter()
+        .enumerate()
         .filter(|(_, c)| **c == b'@')
-        .filter(|(p, _)| {
-            m.neighbours(*p, true)
-                .filter(|n| m[n] == b'@')
+        .filter(|(i, _)| {
+            neighbours(*i, nrows, ncols)
+                .filter(|&p| v[p] == b'@')
                 .count() < 4
         })
         .count()
@@ -32,32 +34,50 @@ fn part_one(input: &str) -> usize
 
 fn part_two(input: &str) -> usize
 {
-    use pathfinding::matrix::Matrix;
-
-    let mut m = Matrix::from_rows(input.lines().map(|l| l.bytes())).unwrap();
+    let mut nrows = 0;
+    let mut ncols = 0;
+    let mut v = input.lines()
+        .inspect(|l| { nrows += 1; ncols = l.len() })
+        .fold(Vec::new(), |mut v, l| { v.extend_from_slice(l.as_bytes()); v });
 
     let mut rolls = 0;
-    let mut v = removeable(&m);
-    while !v.is_empty() {
-        rolls += v.len();
-        v.iter().for_each(|p| m[p] = b'.');
-        v = removeable(&m);
+    let mut v1 = removeable(&v, nrows, ncols);
+    while !v1.is_empty() {
+        rolls += v1.len();
+        v1.iter().for_each(|i| v[*i] = b'.');
+        v1 = removeable(&v, nrows, ncols);
     }
 
     rolls
 }
 
-fn removeable(m: &Matrix<u8>) -> Vec<(usize, usize)>
+fn removeable(v: &[u8], nrows: usize, ncols: usize) -> Vec<usize>
 {
-    m.items()
+    v.iter()
+        .enumerate()
         .filter(|(_, c)| **c == b'@')
-        .filter(|(p, _)| {
-            m.neighbours(*p, true)
-                .filter(|n| m[n] == b'@')
+        .filter(move |(i, _)| {
+            neighbours(*i, nrows, ncols)
+                .filter(|&p| v[p] == b'@')
                 .count() < 4
         })
-        .map(|(p, _)| p)
+        .map(|(i, _)| i)
         .collect()
+}
+
+fn neighbours(i: usize, nrows: usize, ncols: usize) -> impl Iterator<Item = usize>
+{
+    let r = i / ncols;
+    let c = i % ncols;
+    let (rows, cols) = (
+        r.saturating_sub(1)..nrows.min(r + 2),
+        c.saturating_sub(1)..ncols.min(c + 2)
+    );
+
+    rows
+        .flat_map(move |rr| cols.clone().map(move |cc| (rr, cc)))
+        .filter(move |&p| p != (r, c))
+        .map(move |(rr, cc)| rr * ncols + cc)
 }
 
 
